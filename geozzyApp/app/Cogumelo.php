2828ea297631a62c0270fd4bb2cf5b659a410223
pablinhob <pablinho.blanco@gmail.com>
2014-11-21 14:47:29 +0100

    subidos novos archivos app

---
+++ geozzyApp/app/Cogumelo.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class Cogumelo extends CogumeloClass
+{
+
+  public $dependences = array(
+    array(
+     "id" => "formstoneWallpaper",
+     "params" => array("Wallpaper"),
+     "installer" => "bower",
+     "includes" => array("jquery.fs.wallpaper.js", "jquery.fs.wallpaper.css")
+    )
+  );
+  public $includesCommon = array();
+
+
+  function __construct() {
+    parent::__construct();
+
+
+    /*MasterView*/
+    $this->addUrlPatterns( '#^404$#', 'view:MasterView::page404' );
+  }
+
+}
