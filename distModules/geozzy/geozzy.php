38206ed1d1e477fdd4b1e8b7ebbc763711928e62
kewelinho <adrian.kewelinho@gmail.com>
2015-02-27 10:47:15 +0100

    .

---
+++ distModules/geozzy/geozzy.php
@@ -0,0 +1,85 @@
+<?php
+
+Cogumelo::load("coreController/Module.php");
+
+define('MOD_GEOZZY_URL_DIR', 'geozzy');
+
+class geozzy extends Module
+{
+  public $name = "geozzy";
+  public $version = "";
+  public $dependences = array(
+
+
+  );
+  public $includesCommon = array(
+
+  );
+
+  function __construct() {
+    $this->addUrlPatterns( '#^'.MOD_GEOZZY_URL_DIR.'$#', 'view:AdminViewStadistic::main' );
+  }
+
+  static function moduleRc() {
+
+    user::load("model/UserModel.php");
+
+
+    fwrite(STDOUT, "Enter the superAdmin password:\n");
+    $passwd = self::getPassword(true);
+
+    $userData = array(
+      'login' => 'superAdmin',
+      'name' => 'superAdmin',
+      'password' => SHA1($passwd),
+      'email' => '',
+      'role' => 10,
+      'active' => 1
+    );
+    $user = new UserModel($userData);
+    $user->save();
+  }
+
+  /**
+   * Get a password from the shell.
+   * This function works on *nix systems only and requires shell_exec and stty.
+   *
+   * @param  boolean $stars Wether or not to output stars for given characters
+   * @return string
+   */
+  static function getPassword($stars = false)
+  {
+      // Get current style
+      $oldStyle = shell_exec('stty -g');
+
+      if ($stars === false) {
+          shell_exec('stty -echo');
+          $password = rtrim(fgets(STDIN), "\n");
+      } else {
+          shell_exec('stty -icanon -echo min 1 time 0');
+
+          $password = '';
+          while (true) {
+              $char = fgetc(STDIN);
+
+              if ($char === "\n") {
+                  break;
+              } else if (ord($char) === 127) {
+                  if (strlen($password) > 0) {
+                      fwrite(STDOUT, "\x08 \x08");
+                      $password = substr($password, 0, -1);
+                  }
+              } else {
+                  fwrite(STDOUT, "*");
+                  $password .= $char;
+              }
+          }
+      }
+
+      // Reset old style
+      shell_exec('stty ' . $oldStyle);
+
+      // Return the password
+      return $password;
+  }
+}
--- geozzyApp/app/conf/setup.dev.php
+++ geozzyApp/app/conf/setup.dev.php
@@ -123,9 +123,9 @@ define( 'MEDIASERVER_COMPILE_LESS', false );
 global $C_ENABLED_MODULES;
 global $C_INDEX_MODULES;
 
-$C_ENABLED_MODULES = array( 'mediaserver', 'i18nGetLang', 'common', 'devel', 'user', 'filedata', 'admin', 'form', 'table' );
+$C_ENABLED_MODULES = array( 'mediaserver', 'i18nGetLang', 'common', 'devel', 'user', 'filedata', 'geozzy', 'admin', 'form', 'table' );
 // before app/Cogumelo.php execution
-$C_INDEX_MODULES  = array( 'mediaserver', 'i18nGetLang', 'user', 'form', 'admin', 'devel' ); // DEVEL SIEMPRE DE ULTIMO!!!
+$C_INDEX_MODULES  = array( 'mediaserver', 'i18nGetLang', 'user', 'geozzy', 'form', 'admin', 'devel' ); // DEVEL SIEMPRE DE ULTIMO!!!
 
 
 //
