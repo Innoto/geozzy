be623759244dccd4e609ae3e4f7ef25d0368334d
pablinhob <pablinho.blanco@gmail.com>
2015-03-10 14:59:45 +0100

    modelo de taxonomías

---
+++ distModules/geozzy/classes/controller/GeozzyExamplesController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class GeozzyExamplesController {
+
+
+  static function newResource() {
+    
+  }
+    
+}
---
+++ distModules/geozzy/classes/model/TaxonomygroupModel.php
@@ -0,0 +1,27 @@
+<?php
+Cogumelo::load('coreModel/VO.php');
+Cogumelo::load('coreModel/Model.php');
+
+
+
+
+class TaxonomygroupModel extends Model
+{
+  static $tableName = 'geozzy_taxonomygroup';
+  static $cols = array(
+    'id' => array(
+      'type' => 'INT',
+      'primarykey' => true,
+      'autoincrement' => true
+    )
+
+  );
+
+  var $filters = array( );
+
+
+  function __construct( $datarray= array(), $otherRelObj = false ) {
+    parent::__construct( $datarray, $otherRelObj );
+  }
+
+}
