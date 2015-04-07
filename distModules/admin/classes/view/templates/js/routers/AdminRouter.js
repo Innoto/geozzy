e8aac500a7b28dc7e913cb26f660ab954e1e7b00
pablinhob <pablinho.blanco@gmail.com>
2015-04-07 16:55:08 +0200

    faltaba admin router

---
+++ distModules/admin/classes/view/templates/js/routers/AdminRouter.js
@@ -0,0 +1,48 @@
+
+var app = app || {};
+
+var AdminRouter = Backbone.Router.extend({
+
+  routes: {
+    "" : "charts",
+    "charts" : "charts",
+    "user/list" : "userList",
+    "user/create" : "userCreate",    
+    "user/edit/:id" : "userEdit",
+    "role/list" : "roleList", 
+    "role/create" : "roleCreate",    
+    "role/edit/:id" : "roleEdit"
+  },
+
+  // charts
+
+  charts: function() {
+    app.mainView.loadAjaxContent( '/admin/charts' );
+  },
+
+  // User
+  userList: function(){
+    app.mainView.loadAjaxContent( '/admin/user/list' );
+  },
+
+  userCreate: function( ) {
+    app.mainView.loadAjaxContent( '/admin/user/create');    
+  },
+
+  userEdit: function( id ) {
+    app.mainView.loadAjaxContent( '/admin/user/edit/' + id );    
+  },
+
+  // Roles
+  roleList: function(){
+    app.mainView.loadAjaxContent( '/admin/role/list' );
+  },
+
+  roleCreate: function( ) {
+    app.mainView.loadAjaxContent( '/admin/role/create');    
+  },
+
+  roleEdit: function( id ) {
+    app.mainView.loadAjaxContent( '/admin/role/edit/' + id );    
+  }
+});
