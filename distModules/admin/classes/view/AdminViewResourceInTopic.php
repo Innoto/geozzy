8fc8965f561f1a0af72e8d049b9ac0af3e361e5c
mjperez <pepaps@gmail.com>
2015-05-19 13:11:53 +0200

    cambio

--- distModules/admin/classes/view/AdminViewResourceInTopic.php
+++ distModules/admin/classes/view/AdminViewResourceInTopic.php
@@ -36,6 +36,11 @@ class AdminViewResourceInTopic extends AdminViewMaster
     // set id search reference.
     $tabla->setSearchRefId('tableSearch');
 
+      // set table Actions
+    $tabla->setActionMethod(__('Publish'), 'changeStatusPublished', 'updateKey( array( "searchKey" => "id", "searchValue" => $rowId, "changeKey" => "published", "changeValue"=>1 ))');
+    $tabla->setActionMethod(__('Unpublish'), 'changeStatusUnpublished', 'updateKey( array( "searchKey" => "id", "searchValue" => $rowId, "changeKey" => "published", "changeValue"=>0 ))');
+    $tabla->setActionMethod(__('Delete'), 'delete', 'listitems(array("filters" => array("id" => $rowId)))->fetch()->delete()');
+
     // set list Count methods in controller
     $tabla->setListMethodAlias('listItems');
     $tabla->setCountMethodAlias('listCount');
@@ -48,11 +53,16 @@ class AdminViewResourceInTopic extends AdminViewMaster
     $tabla->setCol('id', 'ID');
     $tabla->setCol('type', __('Type'));
     $tabla->setCol('title_'.LANG_DEFAULT, __('Title'));
+    $tabla->setCol('published', __('Published'));
 
     // Filtrar por temática
     $tabla->setDefaultFilters( array('ResourceTopicModel.topic'=> $topic[1] ) );
     $tabla->setAffectsDependences( array('ResourceTopicModel') ) ;
     $tabla->setJoinType('INNER');
+
+      // Contido especial
+    $tabla->colRule('published', '#1#', '<span class=\"rowMark rowOk\"><i class=\"fa fa-circle\"></i></span>');
+    $tabla->colRule('published', '#0#', '<span class=\"rowMark rowNo\"><i class=\"fa fa-circle\"></i></span>');
 
     // imprimimos o JSON da taboa
     $tabla->exec();
