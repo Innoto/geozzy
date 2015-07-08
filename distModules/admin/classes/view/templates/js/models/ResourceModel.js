fea147a8fc76925d15675ef4fa79bb3fe34cf9cc
pablinhob <pablinho.blanco@gmail.com>
2015-07-08 17:36:27 +0200

    reparados problemas collections e engadidos arquivos de explorador

--- distModules/admin/classes/view/templates/js/models/ResourceModel.js
+++ distModules/admin/classes/view/templates/js/models/ResourceModel.js
@@ -2,21 +2,9 @@ var ResourceModel = Backbone.Model.extend({
   defaults: {
     id: false,
     type: false,
-    published: false,
-    title: '',
-    deleted: 0,
-    weight: false
-  },
-  destroyResourceTerm: function (options) {
-    var opts = _.extend({
-        url: '/api/admin/resourcesTerm/'+options+ '/resource/' +this.id
-      },
-      options || {});
-    return Backbone.Model.prototype.destroy.call(this, opts);
-  },
+    latlng: false,
 
-  saveResourceTerm: function (options) {
-    return Backbone.sync('update', this, {url: '/api/admin/resourcesTerm/'+options+ '/resource/' +this.id});
+    fullyLoaded: false
   }
 
 });
