54a60dd56fde86016cde79a467ce37afeac1a457
pablinhob <pablinho.blanco@gmail.com>
2015-08-14 14:18:16 +0200

    Drop view en view resource index

--- distModules/geozzyAPI/classes/model/ResourceIndexModel.php
+++ distModules/geozzyAPI/classes/model/ResourceIndexModel.php
@@ -8,7 +8,9 @@ Cogumelo::load('coreModel/Model.php');
 class ResourceIndexModel extends Model
 {
   var $notCreateDBTable = true;
-  var $rcSQL = "CREATE VIEW geozzy_resource_index AS
+  var $rcSQL = "
+              DROP VIEW IF EXISTS geozzy_resource_index;
+              CREATE VIEW geozzy_resource_index AS
                 SELECT geozzy_resource.id as id, geozzy_resource.rTypeId as rTypeId, geozzy_resource_taxonomyterm.taxonomyterm as taxonomyterm,  geozzy_resource_topic.topic as topic, geozzy_resource.loc as loc from geozzy_resource
                   LEFT JOIN geozzy_resource_taxonomyterm ON geozzy_resource_taxonomyterm.resource = geozzy_resource.id
                   LEFT JOIN geozzy_resource_topic ON geozzy_resource_topic.resource = geozzy_resource.id;
