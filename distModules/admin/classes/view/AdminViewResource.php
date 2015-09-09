e65903d1a6be80c3a6abad3f98750c6a2a2e0767
Jose M. Porto <jmporto@innoto.es>
2015-09-09 16:55:42 +0200

    .

--- distModules/admin/classes/view/AdminViewResource.php
+++ distModules/admin/classes/view/AdminViewResource.php
@@ -151,6 +151,7 @@ error_log( 'resourceEditForm' );
     $tsEntrada = microtime( true );
 
     $recursoData = false;
+    $urlParamTopic = false;
     $topicItem = false;
 
     /* Validamos os parámetros da url e obtemos un array de volta*/
