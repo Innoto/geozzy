05556d7a565ec639da2ca302682131859755fac8
pablinhob <pablinho.blanco@gmail.com>
2015-09-14 16:57:32 +0200

    Cambios para ITG

--- geozzyApp/app/conf/geozzyExplorers.php
+++ geozzyApp/app/conf/geozzyExplorers.php
@@ -4,6 +4,7 @@ global $GEOZZY_EXPLORERS;
 
 
 $GEOZZY_EXPLORERS['default'] = array(
+  'name' => 'Explorador por defecto de geozzy',
   'module' => 'explorer',
   'controllerFile' => 'controller/DefaultExplorerController.php',
   'controllerName' => 'DefaultExplorerController'
