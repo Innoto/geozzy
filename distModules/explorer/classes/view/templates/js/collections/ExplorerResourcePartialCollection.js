93bc59d6558c12450ca00fd4b72322c4ed982262
pablinhob <pablinho.blanco@gmail.com>
2015-10-01 13:05:26 +0200

    fetch selectivo, non se cargan recursos xa cargados

--- distModules/explorer/classes/view/templates/js/collections/ExplorerResourcePartialCollection.js
+++ distModules/explorer/classes/view/templates/js/collections/ExplorerResourcePartialCollection.js
@@ -15,19 +15,24 @@ var ExplorerResourcePartialCollection = Backbone.Collection.extend({
       }
     });
 
-    var tmpResources = resPartialCollection.fetch({
-      data: {ids: params.ids},
-      type: 'POST',
-      success: function( list ) {
-        list.each(function(resource) {
-          that.add(resource);
-        });
-        if(params.success) {
-          params.success();
-        }
-      }
-    });
 
+    if( idsToFetch.length > 0) {
+      var tmpResources = resPartialCollection.fetch({
+        data: {ids: idsToFetch},
+        type: 'POST',
+        success: function( list ) {
+          list.each(function(resource) {
+            that.add(resource);
+          });
+          if(params.success) {
+            params.success();
+          }
+        }
+      });
+    }
+    else {
+      params.success();
+    }
   }
 
 });
