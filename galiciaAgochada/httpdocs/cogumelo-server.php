4fb6277377142044e823c57b029352c722a34f32
Jose M. Porto <jmporto@innoto.es>
2015-10-13 11:54:41 +0200

    Añado isPrivateIp como funcion interna

--- galiciaAgochada/httpdocs/cogumelo-server.php
+++ galiciaAgochada/httpdocs/cogumelo-server.php
@@ -13,10 +13,9 @@ require_once( 'conf/setup.php' );
 
 // We check that the conexion comes from localhost
 if( isset( $_SERVER['REMOTE_ADDR'] ) && $_SERVER['REMOTE_ADDR'] != 'local_shell' ) {
+
+  // SOLO desde servidor con conexion interna
   if( isPrivateIp( $_SERVER['REMOTE_ADDR'] ) ) {
-
-
-
     require_once(COGUMELO_LOCATION.'/coreClasses/CogumeloClass.php');
     require_once(COGUMELO_LOCATION.'/coreClasses/coreController/DependencesController.php');
     require_once(SITE_PATH.'/Cogumelo.php');
@@ -27,25 +26,25 @@ if( isset( $_SERVER['REMOTE_ADDR'] ) && $_SERVER['REMOTE_ADDR'] != 'local_shell'
         $dir = SITE_PATH.'log/';
         $handle = opendir($dir);
         while ($file = readdir($handle)){
-            if (is_file($dir.$file)){
-                $file = $dir.$file;
-                $pos = strpos($file, 'gz');
-                if ($pos==false){
-                    $gzfile = $file.'-'.date('Ymd-Hms').'.gz';
-                    $fp = gzopen($gzfile, 'w9');
-                    gzwrite ($fp, file_get_contents($file));
-                    gzclose($fp);
-                }
+          if (is_file($dir.$file)){
+            $file = $dir.$file;
+            $pos = strpos($file, 'gz');
+            if ($pos==false){
+                $gzfile = $file.'-'.date('Ymd-Hms').'.gz';
+                $fp = gzopen($gzfile, 'w9');
+                gzwrite ($fp, file_get_contents($file));
+                gzclose($fp);
             }
+          }
         }
         break;
       case 'flush':
         $dir = SITE_PATH.'tmp/templates_c/';
         $handle = opendir($dir);
         while ($file = readdir($handle)){
-            if (is_file($dir.$file)){
-                unlink($dir.$file);
-            }
+          if (is_file($dir.$file)){
+            unlink($dir.$file);
+          }
         }
         break;
       case 'client_caches':
@@ -55,5 +54,12 @@ if( isset( $_SERVER['REMOTE_ADDR'] ) && $_SERVER['REMOTE_ADDR'] != 'local_shell'
         CacheUtilsController::generateAllCaches();
 
     }
+
   }
+
+}
+
+
+function isPrivateIp( $ip ) {
+  return( strpos( $ip, '127.' ) === 0 || !filter_var( $ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE ) );
 }
