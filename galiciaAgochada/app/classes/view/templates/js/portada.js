bc3798ab47c04588fca4e4aad17b8c7e2acae091
arodriguez <adrian.kewelinho@gmail.com>
2015-11-27 14:33:17 +0100

    .

--- galiciaAgochada/app/classes/view/templates/js/portada.js
+++ galiciaAgochada/app/classes/view/templates/js/portada.js
@@ -8,8 +8,7 @@ $( document ).ready(function(){
     }
   });
 
-$('.owl-carousel').owlCarousel({
-
+  $('.owl-carousel').owlCarousel({
     loop: true,
     margin:10,
     nav:true,
@@ -25,4 +24,7 @@ $('.owl-carousel').owlCarousel({
         }
     }
   });
+
+  $('.owl-carousel .owl-nav .owl-prev').html('<i class="fa fa-angle-left"></i>');
+  $('.owl-carousel .owl-nav .owl-next').html('<i class="fa fa-angle-right"></i>');
 });
