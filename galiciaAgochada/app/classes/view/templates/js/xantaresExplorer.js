dc9e2f2a708882aea7c0130b5d0ab80abe8095f3
arodriguez <adrian.kewelinho@gmail.com>
2016-01-07 16:08:33 +0100

    .

--- galiciaAgochada/app/classes/view/templates/js/xantaresExplorer.js
+++ galiciaAgochada/app/classes/view/templates/js/xantaresExplorer.js
@@ -28,8 +28,6 @@
       });
 
       interfaceFilters();
-
-
     });
 
 
@@ -38,7 +36,6 @@
       // EXPLORADOR
       var explorer = new geozzy.explorer({
         debug:false,
-        explorerId:'xantares',        
         explorerSectionName:'Paisaxes espectaculares',
         resourceAccess: function(id) {
           //alert('mecagoenmimaquina'+id)
@@ -148,24 +145,7 @@
         '</div>';
         $('.explorer-container-gallery').append(tempElemHtml);
       }
-
-
-
-      $('select.select2GeozzyCustom').select2({
-         minimumResultsForSearch: -1,
-         templateSelection: formatState,
-         templateResult: formatState
-      });
-      //LAYOUT
-      layoutDistributeSize();
-
     }
-
-
-
-    $(window).bind("load resize", function() {
-      layoutDistributeSize();
-    });
 
     function formatState (state) {
 
@@ -185,100 +165,7 @@
       return $ret;
     }
 
-    function layoutDistributeSize(){
-      var hExplorerLayout = $('.paisaxesExplorer').height();
-      var hExplorerFilters = $('.paisaxesExplorer .explorer-container-filter').height();
-      var hExplorerGallery = $('.paisaxesExplorer .explorer-container-gallery').height();
-      var hHeader = 100;
-      var hExplorerMap = hExplorerLayout - (hExplorerGallery + hExplorerFilters + hHeader);
-
-      $('.paisaxesExplorer .explorer-container-map').height( hExplorerMap );
-
-      console.log('hExplorerLayout: ', hExplorerLayout );
-      console.log('hExplorerFilters: ', hExplorerFilters );
-      console.log('hExplorerGallery: ', hExplorerGallery );
-      console.log('hExplorerMap: ', hExplorerMap );
+    function interfaceFilters(){
+      var filterContainer = $(explorerclass+' .explorer-container-filter');
+      console.log(filterContainer);
     }
-
-
-
-
-
-/*
-
-    $(document).ready(function(){
-
-      var explorerclass = '.xantaresExplorer';
-
-
-      // ESTO CHEGARÍA POR CHAMADA AJAX
-      var dataFilter1 = [
-        {value:'*', title: 'Todas'},
-        {value:'10', title: 'Galega swagger'},
-        {value:'11', title: 'Canibal'},
-        {value:'12', title: 'Indo oceánica'}
-      ];
-
-
-
-      // GOOGLE MAPS MAPS
-      var mapOptions = {
-        center: { lat: 43.1, lng: -7.36 },
-        zoom: 8
-      };
-
-      var resourceMap = new google.maps.Map( $( explorerclass+' .explorerMap').get( 0 ), mapOptions);
-
-
-
-      // EXPLORADOR
-      var explorer = new geozzy.explorer({debug:false});
-
-
-      // DISPLAYS
-      var infowindow = new geozzy.explorerDisplay.mapInfoView();
-      //var listaPasiva = new geozzy.explorerDisplay.pasiveListView({ el:$('.explorer-container-gallery')});
-      var mapa = new geozzy.explorerDisplay.mapView({ map: resourceMap, clusterize:false });
-
-
-      //explorer.addDisplay( listaPasiva );
-      explorer.addDisplay( mapa );
-      explorer.addDisplay( infowindow );
-
-      explorer.addFilter(
-        new geozzy.filters.filterSelectSimpleView(
-          {
-            mainCotainerClass: explorerclass+' .explorer-container-filter .explorerFilters',
-            containerClass: 'tipoPaisaxe select2GeozzyCustom',
-            //title:'asdfasfd',
-            data: dataFilter1
-          }
-        )
-      );
-
-      // EXECUCIÓN EXPLORADOR
-      explorer.exec();
-
-      for (var i = 0; i < 12; i++) {
-
-        var tempElemHtml = '<div data-resource-id="" class="col-md-12 element">'+
-          '<div class="elementImg">'+
-            '<img class="img-responsive" src="http://lorempixel.com/530/213/food/'+i+'" />'+
-            '<ul class="elementOptions container-fluid">'+
-              '<li class="elementOpt elementFav">'+
-                '<i class="fa fa-heart-o"></i>'+
-                '<i class="fa fa-heart"></i></li>'+
-            '</ul>'+
-          '</div>'+
-          '<div class="elementInfo">'+
-            '<div class="elementTitle">Lorem ipsum dolor sit amet, consectetur</div>'+
-            '<div class="elementType"><i class="fa fa-cutlery"></i> Furancho</div>'+
-            '<div class="elementPrice">'+(i*103)+'€<span>/persona</span></div>'+
-          '</div>'+
-        '</div>';
-        $('.explorer-container-gallery').append(tempElemHtml);
-      }
-
-
-    });
-*/
