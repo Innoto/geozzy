00ff2955de0f4fc2c171cc682294a4bf6619d382
pablinhob <pablinho.blanco@gmail.com>
2016-01-07 11:21:20 +0100

    tíñamos tabuladores nun view sql

--- galiciaAgochada/app/modules/explorer/classes/model/XantaresExplorerModel.php
+++ galiciaAgochada/app/modules/explorer/classes/model/XantaresExplorerModel.php
@@ -9,35 +9,35 @@ class XantaresExplorerModel extends Model
 {
   var $notCreateDBTable = true;
   var $rcSQL = "
-                DROP VIEW IF EXISTS geozzy_xantares_explorer_index;
-                CREATE VIEW geozzy_xantares_explorer_index AS
-                  SELECT
-                    geozzy_resource.id as id,
-                    geozzy_resourcetype.idName as rtype,
-                    geozzy_resource.title_en as title_en,
-                    geozzy_resource.title_es as title_es,
-                    geozzy_resource.title_gl as title_gl,
-                    geozzy_resource.image as image,
-                    geozzy_resource.shortDescription_es as shortDescription_es,
-                    geozzy_resource.shortDescription_en as shortDescription_en,
-                    geozzy_resource.shortDescription_gl as shortDescription_gl,
-                    geozzy_resource.loc as loc,
-                    geozzy_resource.timeLastUpdate as timeLastUpdate,
-                    group_concat(geozzy_resource_taxonomyterm.taxonomyterm) as terms
-                  FROM geozzy_resource
-                  LEFT JOIN geozzy_resource_taxonomyterm
-                  ON geozzy_resource.id = geozzy_resource_taxonomyterm.resource
-                  LEFT JOIN geozzy_resourcetype
-        				  ON geozzy_resource.rTypeId = geozzy_resourcetype.id
+        DROP VIEW IF EXISTS geozzy_xantares_explorer_index;
+        CREATE VIEW geozzy_xantares_explorer_index AS
+          SELECT
+            geozzy_resource.id as id,
+            geozzy_resourcetype.idName as rtype,
+            geozzy_resource.title_en as title_en,
+            geozzy_resource.title_es as title_es,
+            geozzy_resource.title_gl as title_gl,
+            geozzy_resource.image as image,
+            geozzy_resource.shortDescription_es as shortDescription_es,
+            geozzy_resource.shortDescription_en as shortDescription_en,
+            geozzy_resource.shortDescription_gl as shortDescription_gl,
+            geozzy_resource.loc as loc,
+            geozzy_resource.timeLastUpdate as timeLastUpdate,
+            group_concat(geozzy_resource_taxonomyterm.taxonomyterm) as terms
+          FROM geozzy_resource
+          LEFT JOIN geozzy_resource_taxonomyterm
+          ON geozzy_resource.id = geozzy_resource_taxonomyterm.resource
+          LEFT JOIN geozzy_resourcetype
+          ON geozzy_resource.rTypeId = geozzy_resourcetype.id
 
-                  WHERE
-                    geozzy_resource.published = 1 AND
-                    (
-                      geozzy_resourcetype.idName = 'rtypeAppRestaurant' OR
-                      geozzy_resourcetype.idName = 'rtypeAppFestaPopular' OR
-                      geozzy_resourcetype.idName = 'rtypeHotel'
-                    )
-                  group by geozzy_resource.id;
+          WHERE
+            geozzy_resource.published = 1 AND
+            (
+              geozzy_resourcetype.idName = 'rtypeAppRestaurant' OR
+              geozzy_resourcetype.idName = 'rtypeAppFestaPopular' OR
+              geozzy_resourcetype.idName = 'rtypeHotel'
+            )
+          group by geozzy_resource.id;
               ";
 
   static $tableName = 'geozzy_xantares_explorer_index';
