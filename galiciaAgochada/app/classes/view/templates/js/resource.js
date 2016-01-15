01d12f53cafa86167f5149274e446cfa0f5a6429
Jose M. Porto <jmporto@innoto.es>
2016-01-15 10:51:29 +0100

    .

--- galiciaAgochada/app/classes/view/templates/js/resource.js
+++ galiciaAgochada/app/classes/view/templates/js/resource.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     }
   );
 
-  if( geozzy.resourceMapController ) {
+  if( typeof geozzy.resourceMapController !== 'undefined' ) {
     geozzy.resourceMapController.prepareMap( resourceMapData.lat, resourceMapData.lon, resourceMapData.zoom, resourceMapData.wrapper );
 
     geozzy.resourceMapController.prepareRoutes( resourceMapData.wrapperRoute );
