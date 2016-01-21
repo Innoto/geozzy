515698af9922aafe9e8acf6395713aa2add29c44
Jose M. Porto <jmporto@innoto.es>
2016-01-21 19:37:41 +0100

    Marker

--- distModules/rextMapDirections/classes/view/templates/js/rExtMapDirectionsController.js
+++ distModules/rextMapDirections/classes/view/templates/js/rExtMapDirectionsController.js
@@ -85,9 +85,9 @@ geozzy.rExtMapDirectionsController = {
 
       // add marker
       this.resourceMarker = new google.maps.Marker({
-        map: that.resourceMap,
-        position: new google.maps.LatLng( that.resourceMapInfo.lat, that.resourceMapInfo.lon ),
-        // title: 'Resource location',
+        map: this.resourceMap,
+        position: new google.maps.LatLng( this.resourceMapInfo.lat, this.resourceMapInfo.lng ),
+        title: 'Resource location',
         icon: {
           url: media+'/module/admin/img/geozzy_marker.png', // This marker is 20 pixels wide by 36 pixels high.
           size: new google.maps.Size(30, 36), // The origin for this image is (0, 0).
