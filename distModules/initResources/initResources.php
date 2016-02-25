6a8aafa00af2751a2c53ab81078c9b89be9f6d0b
mjperez <pepaps@gmail.com>
2016-02-25 16:57:13 +0100

    .

--- distModules/initResources/initResources.php
+++ distModules/initResources/initResources.php
@@ -8,14 +8,14 @@ class initResources extends Module {
 
 
   public function __construct() {
-    $this->addUrlPatterns( '#^initResources$#', 'view:InitResourcesView::generateResources' );
+    //$this->addUrlPatterns( '#^initResources$#', 'view:InitResourcesView::generateResources' );
   }
 
 
   public function moduleRc() {
-    /*initResources::load('controller/InitResourcesController.php');
+    initResources::load('controller/InitResourcesController.php');
     $initResources = new InitResourcesController();
-    $initResources->generateResources();*/
+    $initResources->generateResources();
   }
 
 }
