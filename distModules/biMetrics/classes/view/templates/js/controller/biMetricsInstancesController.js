834ae8f2e95dafa9cd2597036e24eedffdc053ed
pablinhob <pablinho.blanco@gmail.com>
2016-03-15 15:39:56 +0100

    biMetrics Instances cambiado de sitio

--- distModules/biMetrics/classes/view/templates/js/controller/biMetricsInstancesController.js
+++
@@ -1,6 +0,0 @@
-var geozzy = geozzy || {};
-
-if(!geozzy.biMetricsInstances) geozzy.biMetricsInstances={};
-
-geozzy.biMetricsInstances.explorer = new geozzy.biMetrics.controller.explorer();
-geozzy.biMetricsInstances.resource = new geozzy.biMetrics.controller.resource();
