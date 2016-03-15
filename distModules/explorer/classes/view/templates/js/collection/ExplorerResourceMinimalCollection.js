26ba7a6aa201cb242cd25a9887e813133404d5b4
pablinhob <pablinho.blanco@gmail.com>
2016-03-15 13:27:08 +0100

    Quedaban comentarios en minimal

--- distModules/explorer/classes/view/templates/js/collection/ExplorerResourceMinimalCollection.js
+++ distModules/explorer/classes/view/templates/js/collection/ExplorerResourceMinimalCollection.js
@@ -3,9 +3,6 @@ var geozzy = geozzy || {};
 if (! geozzy.explorerComponents) { geozzy.explorerComponents= {} }
 
 geozzy.explorerComponents.resourceMinimalCollection = Backbone.Collection.extend({
-//var geozzy.explorerComponents.resourceMinimalCollection = Backbone.Collection.extend({
   url: false,
   model: ExplorerResourceMinimalModel
-
-
 });
