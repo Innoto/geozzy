964f6652710afe2974279089830fcad0f62a7176
Jose M. Porto <jmporto@innoto.es>
2016-04-05 12:23:20 +0200

    Probando MailController

--- galiciaAgochada/app/classes/view/MailExample.php
+++ galiciaAgochada/app/classes/view/MailExample.php
@@ -25,8 +25,18 @@ class MailExample extends View {
 
     // __construct( $vars = false, $template = false, $module = false )
     // $mailControl = new MailController( ['var1'=>'Contido var 1', 'var2'=>'Contido var 2'], 'mailExample.tpl' );
+
     $mailControl = new MailController();
 
+    $tpl = new Template();
+    $tpl->setTpl( 'mailExample.tpl' );
+    $mailControl->setBody( 'Texto sin HTML', $tpl, array( 'var1'=>'Contido var 1', 'var2'=>'2' ) );
+    $mailControl->send( 'jmporto@innoto.es', 'Ola meu - Compacto' );
+
+
+    /*
+
+    Ejemplos variados
 
     $to = 'jmporto@innoto.es';
     //$to = 'guiseris@hotmail.com';
@@ -34,13 +44,13 @@ class MailExample extends View {
 
     // Ej.1: Sin HTML y sin tpls.
     $mailControl->clear();
-    $mailControl->setMailBody( 'Texto sin HTML' );
+    $mailControl->setBody( 'Texto sin HTML' );
     $mailControl->send( $to, 'Ola meu - Ej.1: Sin HTML y sin tpl.' );
 
 
     // Ej.2: Con texto y HTML, indicando From.
     $mailControl->clear();
-    $mailControl->setMailBody( 'Texto sin HTML', '<p>Correo en formato <strong>HTML</strong></p>' );
+    $mailControl->setBody( 'Texto sin HTML', '<p>Correo en formato <strong>HTML</strong></p>' );
     $mailControl->send( $to, 'Ola meu - Ej.2: Con texto y HTML, indicando From', 'Porto Test', 'test@olameu.com' );
 
 
@@ -50,8 +60,8 @@ class MailExample extends View {
     $tpl1->assign( 'var1', 'Contido var 1' );
     $tpl1->assign( 'var2', 'Contido da var 2 en tpl <strong>HTML</strong>' );
     $mailControl->clear();
-    $mailControl->setMailBodyPlain( 'Texto sin HTML' );
-    $mailControl->setMailBodyHtml( $tpl1 );
+    $mailControl->setBodyPlain( 'Texto sin HTML' );
+    $mailControl->setBodyHtml( $tpl1 );
     $mailControl->send( array( $to, 'meu@olameu.com' ), 'Ola meu - Ej.3: Con texto y HTML con tpl' );
 
 
@@ -59,20 +69,20 @@ class MailExample extends View {
     $tpl2 = new Template();
     $tpl2->setTpl( 'mailExample.tpl' );
     $mailControl->clear();
-    $mailControl->setMailBody( 'Texto sin HTML', $tpl2, array( 'var1'=>'Contido var 1', 'var2'=>'2' ) );
+    $mailControl->setBody( 'Texto sin HTML', $tpl2, array( 'var1'=>'Contido var 1', 'var2'=>'2' ) );
     $mailControl->send( $to, 'Ola meu - Ej.4: Compacto', 'Porto Test', 'test@olameu.com' );
 
 
     // Ej.5: HTML sin verisón texto. No recomendable!!!
     $mailControl->clear();
-    $mailControl->setMailBodyHtml( '<p>Correo en formato <strong>HTML</strong></p>' );
+    $mailControl->setBodyHtml( '<p>Correo en formato <strong>HTML</strong></p>' );
     $mailControl->send( $to, 'Ola meu - Ej.5: HTML sin verisón texto. No recomendable' );
 
 
     // Ej.F1: Correo con 1 fichero
     $filePath = ModuleController::getRealFilePath( 'classes/view/templates/img/aloxamentos.png' ); // Fich. en APP
     $mailControl->clear();
-    $mailControl->setMailBody( 'Mira 1 fichero:' );
+    $mailControl->setBody( 'Mira 1 fichero:' );
     $mailControl->setFiles( $filePath );
     $mailControl->send( $to, 'Ola meu - Ej.F1: Correo con 1 fichero' );
 
@@ -83,8 +93,10 @@ class MailExample extends View {
       ModuleController::getRealFilePath( 'classes/view/templates/img/cabecera.png' ) // Fich. en APP
     );
     $mailControl->clear();
-    $mailControl->setMailBody( 'Mira varios ficheros:',  '<p>Mira varios <strong>ficheros</strong>:</p>' );
+    $mailControl->setBody( 'Mira varios ficheros:',  '<p>Mira varios <strong>ficheros</strong>:</p>' );
     $mailControl->setFiles( $files );
     $mailControl->send( $to, 'Ola meu - Ej.F2: Correo con varios ficheros' );
+
+    */
   }
 }
