4b10cf7b989f1a56e9866d00d6101f6db2421c04
pablinhob <pablinho.blanco@gmail.com>
2016-04-21 13:54:05 +0200

    Código de idioma xa está para todos os recusos, eliminoa nestes exploradores

--- galiciaAgochada/app/classes/view/templates/js/todosSegredosExplorer.js
+++ galiciaAgochada/app/classes/view/templates/js/todosSegredosExplorer.js
@@ -14,26 +14,6 @@
       //$( '.explorerFilterElement .especialidadeEatandDrink').select2();
     });
 
-    // autodetección de idioma
-    path = window.location.pathname.split('/');
-
-    has_lang = false;
-    $(langAvailableIds).each(function(i,e){
-      if (path[1]===e){
-        has_lang = true;
-      }
-    });
-
-    if(!has_lang){ // url sen idioma
-      if (navigator.appName == 'Netscape' || 'Microsoft Internet Explorer' || 'Opera'){
-        var idioma = navigator.language;
-      }
-      else{
-        var idioma = navigator.browserLanguage;
-      }
-      lang_array = idioma.split('-');
-      window.location = lang_array[0] + window.location.pathname;
-    }
 
   });
 
