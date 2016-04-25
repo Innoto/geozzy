87b34b3476eb68102067b89df0f3f91bc4c3e88a
arodriguez <adrian.kewelinho@gmail.com>
2016-04-25 17:50:51 +0200

    borrar modulo comment

--- distModules/comment/classes/view/templates/comment.tpl
+++
@@ -1,6 +0,0 @@
-{$commentFormOpen}
-  {foreach from=$commentFormFields key=key item=field}
-    {$field}
-  {/foreach}
-{$commentFormClose}
-{$commentFormValidations}
--- distModules/comment/classes/view/templates/js/CommentInstance.js
+++
@@ -1,6 +0,0 @@
-
-var geozzy = geozzy || {};
-geozzy.commentInstance = new geozzy.comment();
-
-geozzy.commentInstance.createComment(15);
-//geozzy.commentInstance.createComment(15,"comment");
