6df668dac6329a03dbdf6e72a63e34f294bf80cb
pablinhob <pablinho.blanco@gmail.com>
2016-04-27 11:58:32 +0200

    Controlamos as instancias de metricas para que non se repitan

--- distModules/biMetrics/classes/view/templates/js/biMetricsInstances.js
+++ distModules/biMetrics/classes/view/templates/js/biMetricsInstances.js
@@ -2,5 +2,12 @@ var geozzy = geozzy || {};
 
 if(!geozzy.biMetricsInstances) geozzy.biMetricsInstances={};
 
-geozzy.biMetricsInstances.explorer = new geozzy.biMetricsComponents.explorer();
-geozzy.biMetricsInstances.resource = new geozzy.biMetricsComponents.resource();
+
+if(typeof geozzy.biMetricsInstances.explorer  === 'undefined'){
+  geozzy.biMetricsInstances.explorer = new geozzy.biMetricsComponents.explorer();
+}
+
+
+if(typeof geozzy.biMetricsInstances.resource  === 'undefined'){
+  geozzy.biMetricsInstances.resource = new geozzy.biMetricsComponents.resource();
+}
