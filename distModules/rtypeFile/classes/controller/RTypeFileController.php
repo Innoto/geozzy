92703e9aae9d6ae153dd6cb009fe2a8c3d02955d
arodriguez <adrian.kewelinho@gmail.com>
2016-05-03 12:23:51 +0200

    .

--- distModules/rtypeFile/classes/controller/RTypeFileController.php
+++ distModules/rtypeFile/classes/controller/RTypeFileController.php
@@ -166,11 +166,36 @@ class RTypeFileController extends RTypeController implements RTypeInterface {
 
     if( !$form->existErrors() ) {
       //error_log( "rExtCtrl->rExtModel:" . print_r( $this->rExtCtrl->rExtModel, true ) );
+      $fileCtrl = new RExtFileController( $this );
+      $valueFile = $form->getFieldValue( $fileCtrl->addPrefix( 'file' ) );
+      $valueImage = $form->getFieldValue( 'image' );
+
+      if(!$valueImage && ( $valueFile && strpos($valueFile['validate']['type'], 'image/') !== false )){
+
+
+        $rexData = $fileCtrl->getRExtData($resource->getter('id'));
+        $resdata['id'] = $resource->getter('id');
+        $resdata['image'] = $rexData['file']['id'];
+        $resModel = new resourceModel($resdata);
+        $resModel->save();
+      }
+      // image field == file field
+    }
+
+    /*
+    if( !$form->existErrors() ) {
+      $this->rExtCtrl = $this->newRExtContr();
+      $this->rExtCtrl->resFormProcess( $form, $resource );
+    }
+    if( !$form->existErrors() ) {
 
       $valueFile = $form->getFieldValue( $this->rExtCtrl->addPrefix( 'file' ) );
       $valueImage = $form->getFieldValue( 'image' );
 
       if(!$valueImage && ( $valueFile && strpos($valueFile['validate']['type'], 'image/') !== false )){
+
+        var_dump($this->defResCtrl->resObj->getter('id'));
+        exit;
         $resdata['id'] = $this->rExtCtrl->rExtModel->getter('resource');
         $resdata['image'] = $this->rExtCtrl->rExtModel->getter('file');
         $resModel = new resourceModel($resdata);
@@ -178,6 +203,7 @@ class RTypeFileController extends RTypeController implements RTypeInterface {
       }
       // image field == file field
     }
+    */
   }
 
   /**
