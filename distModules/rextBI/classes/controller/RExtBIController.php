d12f706fb0a45a60d86f26f3f96c24634b8c90f3
pablinhob <pablinho.blanco@gmail.com>
2016-05-11 17:18:47 +0200

    extensión BI para parte pública

---
+++ distModules/rextBI/classes/controller/RExtBIController.php
@@ -0,0 +1,106 @@
+<?php
+
+
+class RExtMapController extends RExtController implements RExtInterface {
+
+  public function __construct( $defRTypeCtrl ){
+    parent::__construct( $defRTypeCtrl, new rextMapDirections(), 'rExtBI_' );
+  }
+
+  /**
+   * Carga los datos de los elementos de la extension
+   *
+   * @param $resId integer
+   *
+   * @return array OR false
+   */
+  public function getRExtData( $resId = false ) {
+    // error_log( "RExtMapDirectionsController: getRExtData( $resId )" );
+    $rExtData = false;
+
+    // error_log( 'RExtMapDirectionsController: getRExtData = '.print_r( $rExtData, true ) );
+    return $rExtData;
+  }
+
+
+  /**
+   * Defino la parte de la extension del formulario
+   *
+   * @param $form FormController
+   */
+  public function manipulateForm( FormController $form ) {
+    // error_log( "RExtMapDirectionsController: manipulateForm()" );
+    $rExtFieldNames = array();
+
+    return( $rExtFieldNames );
+  } // function manipulateForm()
+
+
+
+  /**
+   * Preparamos los datos para visualizar la parte de la extension del formulario
+   *
+   * @param $form FormController
+   *
+   * @return Array $viewBlockInfo{ 'template' => array, 'data' => array, 'dataForm' => array }
+   */
+  // parent::getFormBlockInfo( $form );
+
+
+
+  /**
+   * Validaciones extra previas a usar los datos
+   *
+   * @param $form FormController
+   */
+  // parent::resFormRevalidate( $form );
+
+
+
+  /**
+   * Creación-Edición-Borrado de los elementos de la extension
+   *
+   * @param $form FormController
+   * @param $resource ResourceModel
+   */
+  public function resFormProcess( FormController $form, ResourceModel $resource ) {
+    // error_log( "RExtMapDirectionsController: resFormProcess()" );
+  }
+
+
+
+  /**
+   * Retoques finales antes de enviar el OK-ERROR a la BBDD y al formulario
+   *
+   * @param $form FormController
+   * @param $resource ResourceModel
+   */
+  // parent::resFormSuccess( $form, $resource )
+
+
+  /**
+   * Preparamos los datos para visualizar la parte de la extension
+   *
+   * @return Array $rExtViewBlockInfo{ 'template' => array, 'data' => array }
+   */
+  public function getViewBlockInfo() {
+/*
+    $rExtViewBlockInfo = parent::getViewBlockInfo();
+
+    $resData = $this->defResCtrl->getResourceData();
+
+    if( isset( $resData['locLat'] ) && $resData['locLat'] !== '' ) {
+      $rExtViewBlockInfo['data']['title'] = $resData['title'];
+      $rExtViewBlockInfo['data']['locLat'] = $resData['locLat'];
+      $rExtViewBlockInfo['data']['locLon'] = $resData['locLon'];
+      $rExtViewBlockInfo['data']['defaultZoom'] = $resData['defaultZoom'];
+
+      $rExtViewBlockInfo['template']['full'] = new Template();
+      $rExtViewBlockInfo['template']['full']->assign( 'rExt', array( 'data' => $rExtViewBlockInfo['data'] ) );
+      $rExtViewBlockInfo['template']['full']->setTpl( 'rExtViewBlock.tpl', 'rextMap' );
+    }
+
+    return $rExtViewBlockInfo;*/
+  }
+
+} // class RExtMapDirectionsController
---
+++ distModules/rextBI/rextBI.php
@@ -0,0 +1,34 @@
+<?php
+Cogumelo::load( 'coreController/Module.php' );
+
+
+class rextBI extends Module {
+
+  public $name = 'rextBI';
+  public $version = 1.0;
+
+
+  public $models = array();
+
+  public $taxonomies = array();
+
+  public $dependences = array();
+
+  public $includesCommon = array(
+    'controller/RExtBIController.php',
+    'js/rExtBIController.js',
+    'js/rExtBIInstance.js'
+  );
+
+
+  public function __construct() {
+  }
+
+
+  public function moduleRc() {
+    geozzy::load( 'controller/RTUtilsController.php' );
+
+    $rtUtilsControl = new RTUtilsController(__CLASS__);
+    $rtUtilsControl->rExtModuleRc();
+  }
+}
