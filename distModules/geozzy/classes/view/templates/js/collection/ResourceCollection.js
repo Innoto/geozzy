f367e0ca8d5e94d199bbf352e9bf04bc815f8087
pablinhob <pablinho.blanco@gmail.com>
2016-05-13 13:01:52 +0200

    colección contra api recursos

--- distModules/geozzy/classes/view/templates/js/collection/ResourceCollection.js
+++ distModules/geozzy/classes/view/templates/js/collection/ResourceCollection.js
@@ -2,8 +2,52 @@ var geozzy = geozzy || {};
 if(!geozzy.collection) geozzy.collection={};
 
 geozzy.collection.ResourceCollection = Backbone.Collection.extend({
-  baseUrl: '/api/resource',
+  baseUrl: '/api/core/resourcelist',
   url: false,
   model: geozzy.model.ResourceModel,
-  sortKey: 'weight'
+  //sortKey: 'weight',
+
+  options: {
+    fields: false,
+    filters: false,
+    rtype: false,
+    rextmodels: false,
+    category: false,
+    collection: false,
+    updatedfrom: false
+  },
+
+  initialize: function( opts ) {
+
+
+    var that = this;
+
+    that.options = $.extend(true, {}, that.options, opts);
+
+    that.url = baseUrl;
+
+    that.url += '/fields/'+that.options.fields
+    that.url += '/filters/'+that.options.filters
+    that.url += '/rtype/'+that.options.rtype
+    that.url += '/rextmodels/'+that.options.rextmodels
+    that.url += '/category/'+that.options.category
+    that.url += '/collection/'+that.options.collection
+    that.url += '/updatedfrom/'+that.options.updatedfrom
+  },
+
+
+  fetchByIds: function( idsArray, fetchCallback ) {
+    var that = this;
+
+    that.fetch({
+      type: 'POST',
+      data:{ids: idsArray},
+      success: function( list ) {
+        fetchCallback();
+      }
+    });
+
+
+  }
+
 });
