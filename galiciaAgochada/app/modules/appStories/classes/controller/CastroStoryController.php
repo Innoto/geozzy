5961c005aafcf74449a0dfa2896eb2926d7a8023
Pablo Blanco <pablinho.blanco@gmail.com>
2016-09-20 16:53:02 +0200

    Update CastroStoryController.php

--- galiciaAgochada/app/modules/appStories/classes/controller/CastroStoryController.php
+++ galiciaAgochada/app/modules/appStories/classes/controller/CastroStoryController.php
@@ -1,9 +1,14 @@
 <?php
 
 rtypeStory::load('controller/StoryController.php');
+rtypeStoryStep::load('model/AllstoriesViewModel.php');
 
 class CastroStoryController extends StoryController {
   function serveStory( $idName ) {
+    // CONTROL PARAMETROS
     
+    // INSTANCIA + CONSULTA
+    
+    // FORMACION DA ESTRUCTURA DE DATOS
   }
 }
