99a1ab08b570b593a6940194a7f55358ca154374
pablinhob <pablinho.blanco@gmail.com>
2016-09-27 10:17:56 +0200

    apariencia da lista: agora con imaxes

--- distModules/rtypeStory/classes/view/templates/js/model/StoryStepModel.js
+++ distModules/rtypeStory/classes/view/templates/js/model/StoryStepModel.js
@@ -4,6 +4,7 @@ if (! geozzy.storyComponents) { geozzy.storyComponents= {} }
 geozzy.storyComponents.StoryStepModel = Backbone.Model.extend({
   defaults: {
     id: false,
+    img: false,
     title: false,
     description: false
   }
