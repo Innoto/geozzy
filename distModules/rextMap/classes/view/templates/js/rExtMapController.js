812da2675cd946bec13bcc957d7bd74da923f962
pablinhob <pablinho.blanco@gmail.com>
2016-10-18 13:11:09 +0200

    sintaxis

--- distModules/rextMap/classes/view/templates/js/rExtMapController.js
+++ distModules/rextMap/classes/view/templates/js/rExtMapController.js
@@ -56,7 +56,7 @@ geozzy.rExtMapController = function( opts ) {
 
     } // if( $mapContainer.length )
     else {
-      console.log( 'rextMap - NOTICE: I cant find map wrapper );
+      console.log( 'rextMap - NOTICE: I cant find map wrapper' );
     }
   };
 
