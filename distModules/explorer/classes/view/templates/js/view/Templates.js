78dc9bd8ef2a3c199f4fef5be6c00685ac69a727
pablinhob <pablinho.blanco@gmail.com>
2016-11-24 17:13:18 +0100

    retirado corazón

--- distModules/explorer/classes/view/templates/js/view/Templates.js
+++ distModules/explorer/classes/view/templates/js/view/Templates.js
@@ -6,7 +6,6 @@ geozzy.explorerComponents.mapInfoViewTemplate = ''+
   '<div class="gempiContent">'+
     '<div class="gempiImg">'+
       '<img class="img-responsive" src="'+cogumelo.publicConf.mediaHost+'cgmlImg/<%-img%>/fast_cut/<%-img%>.jpg" />'+
-      '<div class="gempiFav"><% if(touchAccess){ %><i class="fa fa-heart-o"></i><i class="fa fa-heart"></i> <% } %></div>'+
     '</div>'+
     '<div class="gempiInfo">'+
       '<div class="gempiTitle"><%-title%></div>'+
