f27693dd2205b2ee4ef30d8c77c312048c400822
pablinhob <pablinho.blanco@gmail.com>
2017-01-23 16:25:25 +0100

    Soporte para rexistro de API

--- distModules/geozzyAPI/geozzyAPI.php
+++ distModules/geozzyAPI/geozzyAPI.php
@@ -24,4 +24,15 @@ class geozzyAPI extends Module {
     $this->addUrlPatterns( '#^api/?$#', 'view:DocAPIView::main' );
     $this->addUrlPatterns( '#^api/doc/index.json$#', 'view:DocAPIView::apidocJson' ); // Main swagger JSON
   }
+
+
+  static function addModuleAPI( $moduleToAddApi ) {
+    global $GEOZZY_API_DOC_URLS;
+    if( !is_array($GEOZZY_API_DOC_URLS) ){
+      $GEOZZY_API_DOC_URLS = [];
+    }
+
+    eval( '$GEOZZY_API_DOC_URLS = array_merge( $GEOZZY_API_DOC_URLS, '.$moduleToAddApi.'::getGeozzyDocAPI() );' );
+
+  }
 }
