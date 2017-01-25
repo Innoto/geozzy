89a3ea671f313ada25305a4781abbb2da0316f9a
pablinhob <pablinho.blanco@gmail.com>
2017-01-25 12:38:55 +0100

    Activado/desactivado de API

--- distModules/geozzyAPI/geozzyAPI.php
+++ distModules/geozzyAPI/geozzyAPI.php
@@ -36,20 +36,18 @@ class geozzyAPI extends Module {
   function addUrlPatternsAPI() {
     global $C_INDEX_MODULES;
     global $COGUMELO_INSTANCED_MODULES;
+    global $GEOZZY_API_ALLOWED_MODULES;
 
     if( count($C_INDEX_MODULES)>0 ) {
 
       foreach($C_INDEX_MODULES as $modName) {
-        //echo $modName;
-
-        if( isset($COGUMELO_INSTANCED_MODULES[$modName])) {
-
-
-          if( method_exists($COGUMELO_INSTANCED_MODULES[$modName], 'setGeozzyUrlPatternsAPI')){
-            $COGUMELO_INSTANCED_MODULES[$modName]->setGeozzyUrlPatternsAPI();
-          }
+        if(
+          isset($COGUMELO_INSTANCED_MODULES[$modName]) &&
+          method_exists($COGUMELO_INSTANCED_MODULES[$modName], 'setGeozzyUrlPatternsAPI') &&
+          in_array($modName, $GEOZZY_API_ALLOWED_MODULES)
+        ) {
+          $COGUMELO_INSTANCED_MODULES[$modName]->setGeozzyUrlPatternsAPI();
         }
-
       }
 
     }
@@ -59,11 +57,18 @@ class geozzyAPI extends Module {
 
   static function addModuleAPI( $moduleToAddApi ) {
     global $COGUMELO_INSTANCED_MODULES;
+    global $GEOZZY_API_ALLOWED_MODULES;
     global $GEOZZY_API_DOC_URLS;
 
     if( !is_array($GEOZZY_API_DOC_URLS) ){
       $GEOZZY_API_DOC_URLS = [];
     }
+    if( !is_array($GEOZZY_API_ALLOWED_MODULES) ){
+      $GEOZZY_API_ALLOWED_MODULES = [];
+    }
+
+    $GEOZZY_API_ALLOWED_MODULES[] = $moduleToAddApi;
+
     $m = $COGUMELO_INSTANCED_MODULES[$moduleToAddApi];
     $GEOZZY_API_DOC_URLS = array_merge( $GEOZZY_API_DOC_URLS, $m->getGeozzyDocAPI() );
 
