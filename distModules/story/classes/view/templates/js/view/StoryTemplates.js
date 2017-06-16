22b075fed2547423baa9fa9b624eb836ad711eef
pablinhob <pablinho.blanco@gmail.com>
2017-06-16 14:31:38 +0200

    sintaxis en templates

--- distModules/story/classes/view/templates/js/view/StoryTemplates.js
+++ distModules/story/classes/view/templates/js/view/StoryTemplates.js
@@ -22,8 +22,8 @@ geozzy.storyComponents.InfowindowPOI = ''+
       '</div>'+
       '<div class="poiInfo">'+
         '<div class="poiTitle"><p><%-title%></p></div>'+
-        '<div class="poiDescription"><%=description%></div>'+
-        '<% if( isNormalResource == 1 ) { %> <a target="_blank" href="/resource/<%-id%>" ><button class="btn btn-primary accessButton">' + __('Discover') + '</button> </a><% }%>'
+        '<div class="poiDescription"><%-description%></div>'+
+        '<% if( isNormalResource == 1 ) { %> <a target="_blank" href="/resource/<%-id%>" ><button class="btn btn-primary accessButton">' + __('Discover') + '</button> </a><% }%>'+
 
       '</div>'+
     '</div>';
