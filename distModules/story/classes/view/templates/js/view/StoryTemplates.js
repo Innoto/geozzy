c7d79d946c678859847fd893549c79981ff6a6ed
pablinhob <pablinho.blanco@gmail.com>
2017-06-16 12:25:35 +0200

    problemas story

--- distModules/story/classes/view/templates/js/view/StoryTemplates.js
+++ distModules/story/classes/view/templates/js/view/StoryTemplates.js
@@ -22,7 +22,7 @@ geozzy.storyComponents.InfowindowPOI = ''+
       '</div>'+
       '<div class="poiInfo">'+
         '<div class="poiTitle"><p><%-title%></p></div>'+
-        '<div class="poiDescription"><%-description%></div>'+
+        '<div class="poiDescription"><%=description%></div>'+
         '<% if( isNormalResource == 1 ) { %> <a target="_blank" href="/resource/<%-id%>" ><button class="btn btn-primary accessButton">' + __('Discover') + '</button> </a><% }%>'
 
       '</div>'
