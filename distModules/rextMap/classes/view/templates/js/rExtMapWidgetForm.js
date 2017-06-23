7236b19a2848375fa84d5cd784771bef1dff7cc6
pablinhob <pablinho.blanco@gmail.com>
2017-06-23 15:25:19 +0200

    situación de marker automático ó buscar addresss

--- distModules/rextMap/classes/view/templates/js/rExtMapWidgetForm.js
+++ distModules/rextMap/classes/view/templates/js/rExtMapWidgetForm.js
@@ -126,12 +126,19 @@ geozzy.rExtMapWidgetForm = function( segmentDIV ) {
     var place = that.autocomplete.getPlace();
     console.log(place.geometry.location.lat(), place.geometry.location.lng() );
 
-    that.resourceMap.setCenter(
-      new google.maps.LatLng(
-        place.geometry.location.lat(),
-        place.geometry.location.lng()
-      )
-    );
+    var pos = new google.maps.LatLng(
+      place.geometry.location.lat(),
+      place.geometry.location.lng()
+    )
+
+    that.resourceMap.setCenter( pos );
+
+
+    that.resourceMarker.setPosition( pos );
+    that.resourceMarker.setMap( that.resourceMap );
+    that.latInput.val( that.resourceMarker.position.lat() );
+    that.lonInput.val( that.resourceMarker.position.lng() );
+
     that.resourceMap.setZoom(11);
 
   };
