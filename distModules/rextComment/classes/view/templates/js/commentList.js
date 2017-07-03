92dd41d7329497e0919952cbb434c451d15a7955
Adrian <adrian.kewelinho@gmail.com>
2017-07-03 14:32:43 +0200

    ,

--- distModules/rextComment/classes/view/templates/js/commentList.js
+++ distModules/rextComment/classes/view/templates/js/commentList.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  console.log($(".rExtCommentList"));
+  console.log($(".CommentList.js"));
   var idResource = $('.resource').attr('data-resource');
   if(idResource){
     geozzy.commentInstance.listComment(idResource);
