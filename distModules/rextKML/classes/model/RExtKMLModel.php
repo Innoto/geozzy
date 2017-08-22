48f2ac5f8f4ad5dbf31f37db2fc32d524046d5fd
pablinhob <pablinho.blanco@gmail.com>
2017-08-22 14:18:53 +0200

    rextKML

---
+++ distModules/rextKML/classes/model/RExtKMLModel.php
@@ -0,0 +1,39 @@
+<?php
+Cogumelo::load( 'coreModel/VO.php' );
+Cogumelo::load( 'coreModel/Model.php' );
+
+
+class RExtKMLModel extends Model {
+
+  static $tableName = 'geozzy_resource_rext_file';
+  static $cols = array(
+    'id' => array(
+      'type' => 'INT',
+      'primarykey' => true,
+      'autoincrement' => true
+    ),
+    'resource' => array(
+      'type'=>'FOREIGN',
+      'vo' => 'ResourceModel',
+      'key' => 'id'
+    ),
+    'file' => array(
+      'type'=>'FOREIGN',
+      'vo' => 'FiledataModel',
+      'key' => 'id',
+      'uploadDir'=> '/rextKML/'
+    ),
+    'author' => array(
+      'type' => 'VARCHAR',
+      'size' => 500
+    )
+  );
+
+  static $extraFilters = array();
+
+
+  public function __construct( $datarray = array(), $otherRelObj = false ) {
+    parent::__construct( $datarray, $otherRelObj );
+  }
+
+}
