36783d2793f6611d1b574db05e28bd5e222e0ade
pablinhob <pablinho.blanco@gmail.com>
2017-08-29 09:46:09 +0200

    test kml con alert

--- distModules/rextKML/classes/view/templates/js/rextKMLView.js
+++ distModules/rextKML/classes/view/templates/js/rextKMLView.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
 
   if( typeof geozzy.rExtMapInstance.resourceMap != 'undefined') {
-
+alert(rextKMLFile);
     var kmlLayer = new google.maps.KmlLayer(rextKMLFile, {
       suppressInfoWindows: true,
       preserveViewport: false,
