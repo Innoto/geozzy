d0b25509af056c2f1f3a18493f0bf43f83dcec1b
unknown <unknown@example.com>
2017-10-11 12:23:02 +0200

    .

--- distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
+++ distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
@@ -1,7 +1,5 @@
   $(document).ready(function(){
-    alert('what?')
     var all_url = window.location.href.split('?');
-    alert(all_url[1])
     var url = all_url[1].split('=');
     if (url[0]=="autoplay" && url[1]=="true"){
       $('.audioguia audio').attr('autoplay', true);
