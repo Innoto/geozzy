269cc81046e58e6c4c5ab244f16c7c447f40f083
unknown <unknown@example.com>
2017-10-13 09:57:23 +0200

    prueba

--- distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
+++ distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
@@ -1,7 +1,11 @@
   $(document).ready(function(){
+    alert(window.location.href)
     var all_url = window.location.href.split('?');
     var url = all_url[1].split('=');
+    alert(url[0])
+    alert(url[1])
     if (url[0]=="autoplay" && url[1]=="true"){
+      alert('entra')
       $('.audioguia audio').attr('autoplay', true);
     }
   });
