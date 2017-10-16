ce0332b0843808113ec5331411ae0bf761203ab0
Adrian <adrian.kewelinho@gmail.com>
2017-10-16 10:40:48 +0200

    explorer

--- distModules/explorer/classes/view/templates/js/view/ExplorerMapInfoView.js
+++ distModules/explorer/classes/view/templates/js/view/ExplorerMapInfoView.js
@@ -48,7 +48,6 @@ geozzy.explorerComponents.mapInfoView = Backbone.View.extend({
       that.hide(params.id);
     });
 
-
     that.parentExplorer.bindEvent('resourceClick', function( params ){
       //that.show(params.id);
     });
