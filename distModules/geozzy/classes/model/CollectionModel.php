f0ba6bcb62dfd758f28f58b8069265b7f16a4e67
Pablo Blanco <pablinho.blanco@gmail.com>
2018-01-10 08:58:18 +0100

    Fecha autoupdate en geozzy

--- distModules/geozzy/classes/model/CollectionModel.php
+++ distModules/geozzy/classes/model/CollectionModel.php
@@ -47,6 +47,10 @@ class CollectionModel extends Model {
     'timeCreation' => array(
       'type' => 'DATETIME'
     ),
+    'timeLastUpdate' => array(
+      'type' => 'DATETIME',
+      'autoUpdateTime' => true
+    ),    
     'weight' => array(
       'type' => 'SMALLINT',
       'default' => 0
@@ -57,6 +61,11 @@ class CollectionModel extends Model {
 
 
   var $deploySQL = array(
+    array(
+      'version' => 'geozzy#4',
+      'sql'=> 'ALTER TABLE `geozzy_collection`
+        ADD `timeLastUpdate` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL AFTER `timeCreation`;'
+    ),
     array(
       'version' => 'geozzy#1.99',
       'sql'=> 'ALTER TABLE `geozzy_collection`
