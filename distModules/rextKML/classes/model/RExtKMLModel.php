77309a16b0dc8c49c9ce384870e0e1fe48ac73e6
Jose M. Porto <jmporto@innoto.es>
2018-02-15 14:08:38 +0100

    Nuevos filtros en los modelos: idIn, resourceIn

--- distModules/rextKML/classes/model/RExtKMLModel.php
+++ distModules/rextKML/classes/model/RExtKMLModel.php
@@ -25,12 +25,15 @@ class RExtKMLModel extends Model {
     )
   );
 
-  
-
-  static $extraFilters = array();
 
 
-  public function __construct( $datarray = array(), $otherRelObj = false ) {
+  static $extraFilters = [
+    'idIn' => ' geozzy_resource_rext_kml.id IN (?) ',
+    'resourceIn' => ' geozzy_resource_rext_kml.resource IN (?) ',
+  ];
+
+
+  public function __construct( $datarray = [], $otherRelObj = false ) {
     parent::__construct( $datarray, $otherRelObj );
   }
 
