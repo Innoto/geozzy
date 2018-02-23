58ab41d38a09584d86b2fc0f4f4e4b2df4192e22
Pablo Blanco <pablinho.blanco@gmail.com>
2018-02-23 13:26:59 +0100

    DEBUG

--- distModules/admin/classes/view/AdminViewMaster.php
+++ distModules/admin/classes/view/AdminViewMaster.php
@@ -42,8 +42,7 @@ class AdminViewMaster extends View {
   public function accessCheck() {
     $useraccesscontrol = new UserAccessController();
     $user = $useraccesscontrol->getSessiondata();
-    var_dump($user);
-    exit;
+
     $res = true;
     if( !$useraccesscontrol->isLogged() ) {
       if( isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] ){
