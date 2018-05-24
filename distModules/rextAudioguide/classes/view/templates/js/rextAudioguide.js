97d37f5f3be1438a1d4d7bad10c971b7d5fa41f4
Pablo Blanco <pablinho.blanco@gmail.com>
2018-05-24 11:56:28 +0200

    delay para audioguia

--- distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
+++ distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
 
 
+  setTimeout(function(){
+    $('.audioPlayer').html(audioGuideTagSource);
 
-  $('.audioPlayer').html(audioGuideTagSource);
-
-  var all_url = window.location.href.split('?');
-  if(all_url[1]){
-    var url = all_url[1].split('=');
-    if (url[0]=="autoplay" && url[1]=="true"){
-      $('.audioguideBlock audio').attr('autoplay', true)
+    var all_url = window.location.href.split('?');
+    if(all_url[1]){
+      var url = all_url[1].split('=');
+      if (url[0]=="autoplay" && url[1]=="true"){
+        $('.audioguideBlock audio').attr('autoplay', true)
+      }
     }
-  }
+  }, 1000);
+
 
 });
