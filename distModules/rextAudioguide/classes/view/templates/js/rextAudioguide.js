2b0d5de7465c707af0499b7cfa320528f591f542
pepinha <pepaps@gmail.com>
2018-05-24 11:34:05 +0200

    .

--- distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
+++ distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+
+
+  $('.audioPlayer').html(audioGuideTagSource);
+/*
   var all_url = window.location.href.split('?');
   if(all_url[1]){
     var url = all_url[1].split('=');
@@ -7,9 +11,5 @@ $(document).ready(function(){
       $('.audioguideBlock audio').attr('autoplay', true)
     }
   }
-
-  $('.audioPlayer').html(audioGuideTagSource);
-
-
-
+*/
 });
