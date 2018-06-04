43349b5915da40dd2cb805648b9a8fbd7733b371
Jose M. Porto <jmporto@innoto.es>
2018-06-04 12:22:52 +0200

    Comento todo o codigo que non se usa de momento

--- distModules/rtypeLikes/classes/controller/RTypeLikesController.php
+++ distModules/rtypeLikes/classes/controller/RTypeLikesController.php
@@ -37,67 +37,71 @@ class RTypeLikesController extends RTypeController implements RTypeInterface {
    *
    * @return Array $formBlockInfo{ 'template' => array, 'data' => array, 'dataForm' => array, 'ext' => array }
    */
-  // public function getFormBlockInfo( FormController $form ) {
+  public function getFormBlockInfo( FormController $form ) {
 
-  //   // Cargamos la informacion del form, los datos y lanzamos los getFormBlockInfo de las extensiones
-  //   $formBlockInfo = parent::getFormBlockInfo( $form );
-
-  //   $templates = $formBlockInfo['template'];
-
-  //   // TEMPLATE panel principa del form. Contiene los elementos globales del form.
-  //   $templates['formBase'] = new Template();
-  //   $templates['formBase']->setTpl( 'rTypeFormBase.tpl', 'geozzy' );
-  //   $templates['formBase']->assign( 'title', __('Main Resource information') );
-  //   $templates['formBase']->assign( 'res', $formBlockInfo );
-
-  //   $formFieldsNames = array_merge(
-  //     $form->multilangFieldNames( 'title' ),
-  //     $form->multilangFieldNames( 'shortDescription' ),
-  //     $form->multilangFieldNames( 'content' )
-  //   );
-  //   $templates['formBase']->assign( 'formFieldsNames', $formFieldsNames );
+    // NO QUEREMOS MOSTRAR NADA
+    return false;
 
 
-  //   $likesTemplate = false;
-  //   if( $formBlockInfo['data']['id'] ) {
-  //     $likesData = $this->getLikesData( $formBlockInfo['data']['id'] );
+    // // Cargamos la informacion del form, los datos y lanzamos los getFormBlockInfo de las extensiones
+    // $formBlockInfo = parent::getFormBlockInfo( $form );
 
-  //     if( $likesData ) {
-  //       // TEMPLATE panel estado de publicacion
-  //       $likesTemplate = new Template();
-  //       $likesTemplate->setTpl( 'likeAdminFormPanel.tpl', 'rtypeLikes' );
-  //       $likesTemplate->assign( 'title', __( 'Likes' ) );
-  //       $likesTemplate->assign( 'res', $formBlockInfo );
-  //       $likesTemplate->assign( 'likesData', $likesData );
-  //     }
-  //   }
+    // $templates = $formBlockInfo['template'];
 
+    // // TEMPLATE panel principa del form. Contiene los elementos globales del form.
+    // $templates['formBase'] = new Template();
+    // $templates['formBase']->setTpl( 'rTypeFormBase.tpl', 'geozzy' );
+    // $templates['formBase']->assign( 'title', __('Main Resource information') );
+    // $templates['formBase']->assign( 'res', $formBlockInfo );
 
-  //   // TEMPLATE con todos los paneles
-  //   $templates['adminFull'] = new Template();
-  //   $templates['adminFull']->setTpl( 'adminContent-8-4.tpl', 'admin' );
-  //   $templates['adminFull']->assign( 'headTitle', __( 'Edit Resource' ) );
-
-  //   // COL8
-  //   $templates['adminFull']->addToFragment( 'col8', $templates['formBase'] );
-  //   if( $likesTemplate ) {
-  //     $templates['adminFull']->addToFragment( 'col8', $likesTemplate );
-  //   }
-  //   // $templates['adminFull']->addToFragment( 'col8', $templates['likeResources'] );
-  //   // COL4
-  //   $templates['adminFull']->addToFragment( 'col4', $templates['publication'] );
-  //   $templates['adminFull']->addToFragment( 'col4', $templates['info'] );
+    // $formFieldsNames = array_merge(
+    //   $form->multilangFieldNames( 'title' ),
+    //   $form->multilangFieldNames( 'shortDescription' ),
+    //   $form->multilangFieldNames( 'content' )
+    // );
+    // $templates['formBase']->assign( 'formFieldsNames', $formFieldsNames );
 
 
-  //   // TEMPLATE en bruto con todos los elementos del form
-  //   $templates['full'] = new Template();
-  //   $templates['full']->setTpl( 'rTypeFormBlock.tpl', 'geozzy' );
-  //   $templates['full']->assign( 'res', $formBlockInfo );
+    // $likesTemplate = false;
+    // if( $formBlockInfo['data']['id'] ) {
+    //   $likesData = $this->getLikesData( $formBlockInfo['data']['id'] );
 
-  //   $formBlockInfo['template'] = $templates;
+    //   if( $likesData ) {
+    //     // TEMPLATE panel estado de publicacion
+    //     $likesTemplate = new Template();
+    //     $likesTemplate->setTpl( 'likeAdminFormPanel.tpl', 'rtypeLikes' );
+    //     $likesTemplate->assign( 'title', __( 'Likes' ) );
+    //     $likesTemplate->assign( 'res', $formBlockInfo );
+    //     $likesTemplate->assign( 'likesData', $likesData );
+    //   }
+    // }
 
-  //   return $formBlockInfo;
-  // }
+
+    // // TEMPLATE con todos los paneles
+    // $templates['adminFull'] = new Template();
+    // $templates['adminFull']->setTpl( 'adminContent-8-4.tpl', 'admin' );
+    // $templates['adminFull']->assign( 'headTitle', __( 'Edit Resource' ) );
+
+    // // COL8
+    // $templates['adminFull']->addToFragment( 'col8', $templates['formBase'] );
+    // if( $likesTemplate ) {
+    //   $templates['adminFull']->addToFragment( 'col8', $likesTemplate );
+    // }
+    // // $templates['adminFull']->addToFragment( 'col8', $templates['likeResources'] );
+    // // COL4
+    // $templates['adminFull']->addToFragment( 'col4', $templates['publication'] );
+    // $templates['adminFull']->addToFragment( 'col4', $templates['info'] );
+
+
+    // // TEMPLATE en bruto con todos los elementos del form
+    // $templates['full'] = new Template();
+    // $templates['full']->setTpl( 'rTypeFormBlock.tpl', 'geozzy' );
+    // $templates['full']->assign( 'res', $formBlockInfo );
+
+    // $formBlockInfo['template'] = $templates;
+
+    // return $formBlockInfo;
+  }
 
 
   /**
@@ -132,96 +136,100 @@ class RTypeLikesController extends RTypeController implements RTypeInterface {
    * @return Array $viewBlockInfo{ 'template' => array, 'data' => array, 'ext' => array }
    */
   public function getViewBlockInfo( $resId = false ) {
-    $viewBlockInfo = false;
 
-    $userAccessCtrl = new UserAccessController();
-    $userInfo = $userAccessCtrl->getSessiondata();
+    // NO QUEREMOS MOSTRAR NADA
+    return false;
 
-    $resUser = $this->defResCtrl->resObj->getter('user');
+    // $viewBlockInfo = false;
 
-    if( $userInfo && $userInfo['data']['id'] === $resUser ) {
-      // Preparamos los datos para visualizar el Recurso con sus extensiones
-      $viewBlockInfo = parent::getViewBlockInfo( $resId );
+    // $userAccessCtrl = new UserAccessController();
+    // $userInfo = $userAccessCtrl->getSessiondata();
 
-      $viewBlockInfo['data']['title'] = __("Likes");
+    // $resUser = $this->defResCtrl->resObj->getter('user');
 
-      // $template = new Template();
-      $template = $viewBlockInfo['template']['full'];
-      $template->setTpl( 'rTypeViewBlock.tpl', 'rtypeLikes' );
+    // if( $userInfo && $userInfo['data']['id'] === $resUser ) {
+    //   // Preparamos los datos para visualizar el Recurso con sus extensiones
+    //   $viewBlockInfo = parent::getViewBlockInfo( $resId );
 
-      $template->addClientScript( 'js/rExtLikeController.js', 'rextLike' );
+    //   $viewBlockInfo['data']['title'] = __("Likes");
 
-      $likesResources = $this->getLikesResources( $viewBlockInfo['data']['id'] );
-      $likesResourcesInfo = ($likesResources) ? $this->getResourcesInfo( $likesResources ) : false;
-      $template->assign( 'likesResourcesInfo', $likesResourcesInfo );
+    //   // $template = new Template();
+    //   $template = $viewBlockInfo['template']['full'];
+    //   $template->setTpl( 'rTypeViewBlock.tpl', 'rtypeLikes' );
+
+    //   $template->addClientScript( 'js/rExtLikeController.js', 'rextLike' );
+
+    //   $likesResources = $this->getLikesResources( $viewBlockInfo['data']['id'] );
+    //   $likesResourcesInfo = ($likesResources) ? $this->getResourcesInfo( $likesResources ) : false;
+    //   $template->assign( 'likesResourcesInfo', $likesResourcesInfo );
 
 
-      // $template->assign( 'res', array( 'data' => $viewBlockInfo['data'], 'ext' => $viewBlockInfo['ext'] ) );
-      $viewBlockInfo['template']['full'] = $template;
-    }
+    //   // $template->assign( 'res', array( 'data' => $viewBlockInfo['data'], 'ext' => $viewBlockInfo['ext'] ) );
+    //   $viewBlockInfo['template']['full'] = $template;
+    // }
 
-    return $viewBlockInfo;
+    // return $viewBlockInfo;
   }
 
 
   /**
    * UTILS
    */
-  public function getLikesData( $likesId ) {
-    $likesData = false;
+  // public function getLikesData( $likesId ) {
+  //   $likesData = false;
 
-    $likeModel = new LikesViewModel();
-    $likeList = $likeModel->listItems( array( 'filters' => array( 'resourceMain' => $likesId ) ) );
-    if( $likeList ) {
-      $likesData = array();
-      while( $likeObj = $likeList->fetch() ) {
-        $allData = $likeObj->getAllData( 'onlydata' );
-        if( isset( $allData['id'] ) && $allData['id'] !== null ) { // Por si hay col. pero no recursos
-          $likesData[] = $allData;
-        }
-      }
-    }
+  //   $likeModel = new LikesViewModel();
+  //   $likeList = $likeModel->listItems( array( 'filters' => array( 'resourceMain' => $likesId ) ) );
+  //   if( $likeList ) {
+  //     $likesData = array();
+  //     while( $likeObj = $likeList->fetch() ) {
+  //       $allData = $likeObj->getAllData( 'onlydata' );
+  //       if( isset( $allData['id'] ) && $allData['id'] !== null ) { // Por si hay col. pero no recursos
+  //         $likesData[] = $allData;
+  //       }
+  //     }
+  //   }
 
-    return $likesData;
-  }
+  //   return $likesData;
+  // }
 
-  public function getLikesResources( $likesId ) {
-    $likesResources = false;
+  // public function getLikesResources( $likesId ) {
+  //   $likesResources = false;
 
-    $likeModel = new LikesListViewModel();
-    $likeList = $likeModel->listItems( array( 'filters' => array( 'id' => $likesId, 'resourceListNotNull' => true ) ) );
-    $likeObj = ( $likeList ) ? $likeList->fetch() : false;
-    $likeData = ( $likeObj ) ? $likeObj->getAllData( 'onlydata' ) : false;
-    $likesResources = ( isset( $likeData['resourceList'] ) ) ? explode( ',', $likeData['resourceList'] ) : false;
+  //   $likeModel = new LikesListViewModel();
+  //   $likeList = $likeModel->listItems( array( 'filters' => array( 'id' => $likesId, 'resourceListNotNull' => true ) ) );
+  //   $likeObj = ( $likeList ) ? $likeList->fetch() : false;
+  //   $likeData = ( $likeObj ) ? $likeObj->getAllData( 'onlydata' ) : false;
+  //   $likesResources = ( isset( $likeData['resourceList'] ) ) ? explode( ',', $likeData['resourceList'] ) : false;
 
-    return $likesResources;
-  }
+  //   return $likesResources;
+  // }
 
-  public function getResourcesInfo( $resIds ) {
-    geozzy::load('model/ResourceModel.php');
-    $resInfo = array();
+  // public function getResourcesInfo( $resIds ) {
+  //   geozzy::load('model/ResourceModel.php');
+  //   $resInfo = array();
 
-    $perfilLikeImg = empty( Cogumelo::getSetupValue( 'mod:filedata:profile:likePageImg' ) ) ?  'wmdpi4' : 'likePageImg';
+  //   $perfilLikeImg = empty( Cogumelo::getSetupValue( 'mod:filedata:profile:likePageImg' ) ) ?  'wmdpi4' : 'likePageImg';
 
-    $resourceModel = new ResourceViewModel();
-    $resourceList = $resourceModel->listItems( array( 'filters' => array( 'inId' => $resIds, 'published' => 1 ) ) );
-    while( $resObj = $resourceList->fetch() ) {
-      $resId = $resObj->getter('id');
-      $resInfo[ $resId ] = array(
-        'id' => $resObj->getter('id'),
-        'title' => $resObj->getter('title'),
-        'shortDescription' => $resObj->getter('shortDescription'),
-        'image' => $resObj->getter('image'),
-        'imageAKey' => $resObj->getter('imageAKey'),
-        'imageName' => $resObj->getter('imageName'),
-        'perfilLikeImg' => $perfilLikeImg,
-        'url' => $resObj->getter('urlAlias'),
-        'rTypeId' => $resObj->getter('rTypeId')
-      );
-    }
+  //   $resourceModel = new ResourceViewModel();
+  //   $resourceList = $resourceModel->listItems( array( 'filters' => array( 'inId' => $resIds, 'published' => 1 ) ) );
+  //   while( $resObj = $resourceList->fetch() ) {
+  //     $resId = $resObj->getter('id');
+  //     $resInfo[ $resId ] = array(
+  //       'id' => $resObj->getter('id'),
+  //       'title' => $resObj->getter('title'),
+  //       'shortDescription' => $resObj->getter('shortDescription'),
+  //       'image' => $resObj->getter('image'),
+  //       'imageAKey' => $resObj->getter('imageAKey'),
+  //       'imageName' => $resObj->getter('imageName'),
+  //       'perfilLikeImg' => $perfilLikeImg,
+  //       'url' => $resObj->getter('urlAlias'),
+  //       'rTypeId' => $resObj->getter('rTypeId')
+  //     );
+  //   }
 
-    return $resInfo;
-  }
+  //   return $resInfo;
+  // }
 
 
 
