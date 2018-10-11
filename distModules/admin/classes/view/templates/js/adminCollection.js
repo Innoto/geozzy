eeb83470fd9f665e87ebb2b8b85e4d96773e9f20
Adrian <adrian.kewelinho@gmail.com>
2018-10-11 12:30:11 +0200

    .size removed

--- distModules/admin/classes/view/templates/js/adminCollection.js
+++ distModules/admin/classes/view/templates/js/adminCollection.js
@@ -29,7 +29,7 @@ function moveHTML(){
 
   var buttonsToMove = $('.modal .gzzAdminToMove');
 
-  if( buttonsToMove.size() > 0 ){
+  if( buttonsToMove.length > 0 ){
     buttonsToMove.each( function() {
       var that = this;
       var cloneButtonBottom = $(this).clone(true, true);
