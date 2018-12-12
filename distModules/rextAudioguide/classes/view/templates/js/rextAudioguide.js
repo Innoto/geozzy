6aa7cde38d67451f83179a9934144de55b174ad3
Carlos Zas <czas@innoto.es>
2018-12-12 15:57:07 +0100

    comprobacion variable

--- distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
+++ distModules/rextAudioguide/classes/view/templates/js/rextAudioguide.js
@@ -2,13 +2,15 @@ $(document).ready(function(){
 
 
   setTimeout(function(){ // non pode colisionar cos ready
-    $('.audioPlayer').html(audioGuideTagSource);
+    if( typeof audioGuideTagSource != 'undefined' ) {
+      $('.audioPlayer').html(audioGuideTagSource);
+    }
 
     var all_url = window.location.href.split('?');
     if(all_url[1]){
       var url = all_url[1].split('=');
       if (url[0]=="autoplay" && url[1]=="true"){
-        $('.audioguideBlock audio').attr('autoplay', true)
+        $('.audioguideBlock audio').attr('autoplay', true);
       }
     }
   }, 1000);
