7009e3d9499e5b5010575e2d52d25802ff993750
Pablo Blanco <pablinho.blanco@gmail.com>
2018-12-21 09:28:21 +0100

    filtro minimapa

--- distModules/explorerMapFilter/classes/view/templates/js/Templates.js
+++ distModules/explorerMapFilter/classes/view/templates/js/Templates.js
@@ -1,13 +1,7 @@
 
 geozzy.explorerComponents.filterMinimapViewTemplate = "" +
-  "<div class='filtersButton'>"+
-    "<div class='filtersButtonContent'><%= iconHtml %></div>"+
-    "<div class='icon'><i class='fas fa-angle-down' aria-hidden='true'></i></div>"+
+  "<div class='minimap'>"+
+    "<div class='selectedText text-center'></div>"+
+    "<div class='map'></div>"+
   "</div>"+
-  "<div class='boxMinimap'>"+
-    "<div class='minimap'>"+
-      "<div class='selectedText text-center'></div>"+
-      "<div class='map'></div>"+
-    "</div>"+
-    "<div class='filterResetMinimap'><button><%- resetButtonText %></button></div>"+
-  "</div>";
+  "<div class='filterResetMinimap'><button><%- resetButtonText %></button></div>";
