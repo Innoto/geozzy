34bc546cbd28f4029deedeed4e50958afbdd00c5
Pablo Blanco <pablinho.blanco@gmail.com>
2019-02-01 10:47:34 +0100

    adaptando mapas on demmand a resto de módulos

--- distModules/rextKML/classes/view/templates/js/rextKMLView.js
+++ distModules/rextKML/classes/view/templates/js/rextKMLView.js
@@ -2,14 +2,14 @@ $(document).ready( function(){
 
   if( typeof geozzy.rExtMapInstance.resourceMap != 'undefined' || geozzy.rExtMapInstance.resourceMap) {
 
-    new google.maps.KmlLayer(rextKMLFile, {
-      preserveViewport: true,
-      map: geozzy.rExtMapInstance.resourceMap
+    geozzy.rExtMapInstance.onLoad(function(){
+      new google.maps.KmlLayer(rextKMLFile, {
+        preserveViewport: true,
+        map: geozzy.rExtMapInstance.resourceMap
+      });
+
+      cogumelo.log('kml layer', rextKMLFile);
     });
-
-    cogumelo.log('kml layer', rextKMLFile);
-
-    //alert(rextKMLFile);
   }
 
 
