c85a78434f09de9c2988e0cd9533abe0dc8b6cae
Adrian <adrian.kewelinho@gmail.com>
2019-04-22 11:47:09 +0200

    .

--- distModules/rextTravelPlanner/classes/view/templates/js/TravelPlannerApp.js
+++ distModules/rextTravelPlanner/classes/view/templates/js/TravelPlannerApp.js
@@ -31,7 +31,7 @@ geozzy.travelPlanner = function( idTravelPlanner ) {
     rextmodels: true,
     urlAlias: true,
     category:true
-  }
+  };
 
   if(cogumelo.publicConf.rtypeTaxonomygroup.toString() !== ''){
     var arrayRtypeKeys = [];
@@ -209,6 +209,6 @@ geozzy.travelPlanner = function( idTravelPlanner ) {
 
   that.closeResource = function() {
     $('.tpDuResource').hide();
-    $('.tpDuResource').html('')
+    $('.tpDuResource').html('');
   };
 }
