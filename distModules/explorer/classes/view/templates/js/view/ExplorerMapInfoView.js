9b5c0aafa10f4f211ed4d70e023c8ffa28d6d031
Pablo Blanco <pablinho.blanco@gmail.com>
2019-05-29 15:25:56 +0200

    delay afecta a show

--- distModules/explorer/classes/view/templates/js/view/ExplorerMapInfoView.js
+++ distModules/explorer/classes/view/templates/js/view/ExplorerMapInfoView.js
@@ -104,26 +104,11 @@ geozzy.explorerComponents.mapInfoView = Backbone.View.extend({
 
   show: function( id ) {
     var that = this;
-
+    that.ready = id;
     that.createInfoMapDiv();
-
     var resourceInfo = new Backbone.Model(  );
-
     resourceInfo.set(that.parentExplorer.resourceMinimalList.get(id).toJSON());
 
-
-    /*if( id == that.ready) {
-      // se xa está aberto accedemos (para touch)
-      if(that.parentExplorer.explorerTouchDevice) {
-        that.parentExplorer.triggerEvent('resourceClick', {
-          id: id,
-          section: 'Explorer: '+that.parentExplorer.options.explorerSectionName
-        });
-      }
-    }
-    else {*/
-    /*}*/
-    that.ready = id;
     that.parentExplorer.fetchPartialList(
       [id],
       function() {
@@ -162,39 +147,49 @@ geozzy.explorerComponents.mapInfoView = Backbone.View.extend({
 
         $( '#'+that.divId ).html( that.template( element ) );
 
-        if( that.ready == id){
+        that.ready = id;
+        setTimeout(function(){
+            that.ready = id;
           $( '#'+that.divId ).show();
-          that.moveInfoMapDivWhenBehindMouse();
+        }, 50);
 
-          $('#' + that.divId + ' button.buttonTabletClose').on('click', function(ev){
-            that.hide();
+
+        that.moveInfoMapDivWhenBehindMouse();
+
+        $('#' + that.divId + ' button.buttonTabletClose').on('click', function(ev){
+          that.hide();
+        });
+
+        $('#' + that.divId + ' button.buttonTabletAccess').on('click', function(ev){
+          that.hide();
+          //that.parentExplorer.navigateUrl( $(ev.target).attr('dataResourceAccessButton') );
+          //that.parentExplorer.triggerEvent( 'resourceAccess' , {id: id } );
+          that.parentExplorer.triggerEvent('resourceClick',{
+            id: id,
+            section: 'Explorer: '+that.parentExplorer.options.explorerSectionName
           });
 
-          $('#' + that.divId + ' button.buttonTabletAccess').on('click', function(ev){
-            that.hide();
-            //that.parentExplorer.navigateUrl( $(ev.target).attr('dataResourceAccessButton') );
-            //that.parentExplorer.triggerEvent( 'resourceAccess' , {id: id } );
-            that.parentExplorer.triggerEvent('resourceClick',{
-              id: id,
-              section: 'Explorer: '+that.parentExplorer.options.explorerSectionName
-            });
+        });
 
-          });
 
-        }
       }
     );
   },
 
   hide: function() {
+    console.log('hide');
     var that = this;
+
     that.ready = false;
+
     setTimeout(function(){
       if(that.ready == false ) {
         $('#'+that.divId).fadeOut(50);
-        //$('#'+that.divId+ ' *').remove();
+        //$('#'+that.divId).hide();
       }
-    }, 250);
+    }, 200);
+
+
   },
 
   getTopLeftPosition: function() {
