75e77d205a3e84e364f5848c2d2ef4a659a29fb4
Pablo Blanco <pablinho.blanco@gmail.com>
2019-07-16 12:45:42 +0200

    lear last

--- distModules/rextMapPolygon/classes/view/templates/js/adminRextMapPolygon.js
+++ distModules/rextMapPolygon/classes/view/templates/js/adminRextMapPolygon.js
@@ -76,6 +76,11 @@ geozzy.adminRextMapPolygonView  = Backbone.View.extend({
 
     that.updateCornersFromInput();
 
+
+    that.parent.$el.find( '.resourceLocationFrame .locationDialog .polygonFormMap .btnClearLastPolygon').on('click', function(){
+      that.clearLastCorner();
+    });
+
     that.parent.$el.find( '.resourceLocationFrame .locationDialog .polygonFormMap .btnClearPolygon').on('click', function(){
       that.clearAllCorners();
     });
@@ -143,6 +148,12 @@ geozzy.adminRextMapPolygonView  = Backbone.View.extend({
       that.removeCorner();
     }
     that.parent.$el.find( '.resourceLocationFrame .locationDialog .polygonFormMap input.cgmMForm-field-rExtMapPolygon_polygonGeometry').val( '' );
+  },
+
+  clearLastCorner: function() {
+    var that = this;
+    that.removeCorner();
+    that.updateInputFromCorners();
   },
 
   updateInputFromCorners: function() {
@@ -248,215 +259,3 @@ geozzy.adminRextMapPolygonView  = Backbone.View.extend({
   }
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-onLoadMaps( function() {
-  google.maps.event.addListenerOnce( resourceFormMaps[0].resourceMap, "idle", function(ev){
-    var adminRextMapPolygonInstance = new adminRextMapPolygon();
-    adminRextMapPolygonInstance.initialize();
-  });
-});
-
-
-var adminRextMapPolygon = function() {
-  var that = this;
-
-  that.editing = false;
-  that.cornerMarkers = false;
-  that.polygon = false;
-  that.polygonAttributes = { //= new google.maps.Polygon({
-     map: resourceFormMaps[0].resourceMap,
-     //paths: coordinates,
-     strokeColor: "#555555",
-     strokeOpacity: 0.2,
-     strokeWeight: 2,
-     fillColor: "#555555",
-     fillOpacity: 0.2,
-     zIndex: -1,
-     clickable:false
-  };//);
-
-  that.icon = {
-      //path: google.maps.SymbolPath.CIRCLE,
-      path: "M -1 -1 L 1 -1 L 1 1 L -1 1 z",
-      strokeColor: "#555555",
-      strokeOpacity: 0,
-      fillColor: "#555555",
-      fillOpacity: 1,
-      scale: 5
-  };
-
-
-
-  that.marker_options = {
-      //map: map,
-      icon: that.icon,
-      flat: true,
-      draggable: true,
-      raiseOnDrag: false
-  };
-
-
-  that.initialize = function() {
-    that.updateCornersFromInput();
-
-    // events
-    $('.rExtMapPolygon .btnEditPolygon').on('click', function(){
-      that.startEdit();
-
-    });
-
-    $('.rExtMapPolygon .btnEndEditPolygon').on('click', function(){
-      that.stopEdit();
-    });
-
-    $('.rExtMapPolygon .btnClearPolygon').on('click', function(){
-      that.clearAllCorners();
-    });
-
-
-    google.maps.event.addListener( resourceFormMaps[0].resourceMap, "rightclick",function(){
-      that.removeCorner();
-      that.updateInputFromCorners();
-    });
-    google.maps.event.addListener( resourceFormMaps[0].resourceMap, "click", function(ev){
-      that.addCorner(ev.latLng);
-      that.updateInputFromCorners();
-    });
-  };
-
-  that.startEdit = function() {
-    $('.rExtMapPolygon .btnEditPolygon').hide();
-    $('.rExtMapPolygon .desc').show();
-    $('.rExtMapPolygon .btnEndEditPolygon').show();
-    $('.rExtMapPolygon .btnClearPolygon').show();
-
-    resourceFormMaps[0].blockMarker = true;
-    that.editing = true;
-    if( that.cornerMarkers != false) {
-      $.each(that.cornerMarkers, function(i,marker) {
-        marker.setMap(resourceFormMaps[0].resourceMap);
-      });
-      that.polygon.set( 'fillOpacity' , 0.8 );
-      that.polygon.set( 'strokeOpacity' , 0.8 );
-    }
-
-  };
-
-  that.stopEdit = function() {
-    $('.rExtMapPolygon .btnEditPolygon').show();
-    $('.rExtMapPolygon .btnEndEditPolygon').hide();
-    $('.rExtMapPolygon .btnClearPolygon').hide();
-    $('.rExtMapPolygon .desc').hide();
-    resourceFormMaps[0].blockMarker = false;
-    that.editing = false;
-    if( that.cornerMarkers != false) {
-      $.each(that.cornerMarkers, function(i,marker) {
-        marker.setMap(null);
-      });
-      that.polygon.set( 'fillOpacity' , that.polygonAttributes.fillOpacity );
-      that.polygon.set( 'strokeOpacity' , that.polygonAttributes.strokeOpacity );
-    }
-  };
-
-
-  that.addCorner = function( latLng, force ) {
-
-    if( that.editing == true || force == true){
-
-      if( that.polygon != false ) {
-        that.polygon.getPath().push(latLng);
-      }
-      else {
-        that.polygonAttributes.paths = [latLng];
-        that.polygon = new google.maps.Polygon(that.polygonAttributes);
-        that.cornerMarkers = [];
-      }
-
-      var ind = that.polygon.getPath().getArray().length -1;
-
-      that.marker_options.position = latLng;
-      var point = new google.maps.Marker(that.marker_options);
-      if( that.editing ){
-        point.setMap(resourceFormMaps[0].resourceMap);
-      }
-      google.maps.event.addListener(point, "drag", function(ev) {
-        that.polygon.getPath().setAt( ind , ev.latLng);
-        that.updateInputFromCorners();
-      });
-      that.cornerMarkers.push(point);
-
-    }
-  };
-
-
-  that.removeCorner = function( ) {
-    if( that.editing == true ){
-      var deletedMarker = that.cornerMarkers.pop();
-      deletedMarker.setMap(null);
-      that.polygon.getPath().pop();
-    }
-  };
-
-  that.clearAllCorners = function() {
-    while( that.polygon.getPath().getArray().length > 0 ) {
-      that.removeCorner();
-    }
-    $('.rExtMapPolygon input.cgmMForm-field-rExtMapPolygon_polygonGeometry').val( '' );
-  };
-
-  that.updateInputFromCorners = function() {
-
-    $('.rExtMapPolygon input.cgmMForm-field-rExtMapPolygon_polygonGeometry').val(  that.coordsToArrayStr( that.polygon.getPath().getArray()) );
-  };
-
-
-  that.updateCornersFromInput = function() {
-
-    var polygonCornersText = $('.rExtMapPolygon input.cgmMForm-field-rExtMapPolygon_polygonGeometry').val().replace(/ /g,''); // without spaces
-    var polygonCornersArray;
-
-    if( polygonCornersText != '') {
-      eval('polygonCornersArray = new Array(' + polygonCornersText + ');' );
-      $.each( that.arrayToCoords( polygonCornersArray ), function(i,e){
-        that.addCorner(e, true);
-      });
-    }
-  };
-
-
-  that.arrayToCoords = function( arr ) {
-    var cordsArray = [];
-    $.each( arr, function(i,e) {
-      cordsArray.push( new google.maps.LatLng(e[0], e[1]) );
-    });
-    return cordsArray;
-  };
-
-  that.coordsToArrayStr = function( cords ) {
-    var cordsStr = '';
-    var coma = '';
-    $.each( cords, function(i,e) {
-      cordsStr += coma + '[' + e.lat().toFixed(7) + ',' + e.lng().toFixed(7) + ']';
-      coma = ',';
-    });
-    return cordsStr;
-  };
-
-};
-*/
--- distModules/rextMapPolygon/classes/view/templates/rExtFormBlock.tpl
+++ distModules/rextMapPolygon/classes/view/templates/rExtFormBlock.tpl
@@ -1,6 +1,7 @@
 <div class="rExtMapPolygon formBlock">
   <span style="display:none;" class="desc"><b>Left click:</b> add polygon corner.  <b>Right click:</b> delete last corner.</span>
-  <button  class="btnClearPolygon btn btn-warning">Clear</button>
+  <button  class="btnClearLastPolygon btn btn-warning">Clear last corner</button>
+  <button  class="btnClearPolygon btn btn-warning">Clear whole polygon</button>
   {*<input style="display:none;" class="polygonCorners" value="[43.6424498,-7.3304821],[43.6538773,-7.3416401],[43.6459280,-7.3603512],[43.6338777,-7.3479916],[43.6353685,-7.3450100],[43.6424497,-7.3517048],[43.6472943,-7.3410618],[43.6403379,-7.3338520]">*}
   <div style="display:none;">{$rExt.dataForm.formFieldsArray.rExtMapPolygon_polygonGeometry}</div>
 </div>
