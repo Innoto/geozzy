922bb36b20f0bdf6cae9146dc1eea829c604e96f
Pablo Blanco <pablinho.blanco@gmail.com>
2019-07-18 09:25:15 +0200

    redibujar ruta

--- distModules/rextRoutes/classes/view/templates/js/adminRextRoutes.js
+++ distModules/rextRoutes/classes/view/templates/js/adminRextRoutes.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 geozzy.adminRextRoutesViewInstance = false;
 geozzy.adminRextRoutesView = Backbone.View.extend({
   editing: true,
+  routeView: false,
   render: function() {
     var that = this;
 
@@ -64,10 +65,14 @@ geozzy.adminRextRoutesView = Backbone.View.extend({
         $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locEndLat').val(ruta.get('end')[0]);
         $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locEndLon').val(ruta.get('end')[1]);
 
-        var route = new geozzy.rextRoutes.routeView( {
+        if( that.routeView != false ) {
+          that.routeView.hideRoute();
+        }
+        that.routeView = new geozzy.rextRoutes.routeView( {
           map: resourceMap,
           routeModel: ruta
-        } );
+        });
+                
       }
     } );
   },
@@ -99,45 +104,3 @@ geozzy.adminRextRoutesView = Backbone.View.extend({
 
 
 });
-
-
-/*
-var adminRextRoutesJs = {
-  fileUpload: function( idForm, fieldName ) {
-    var that = this;
-    var routesCollection = new geozzy.rextRoutes.routeCollection();
-
-    routesCollection.url = '/api/adminRoutes/idForm/' + $('input[name=cgIntFrmId][form='+idForm+']').val();
-    routesCollection.model= geozzy.rextRoutes.routeModel;
-    routesCollection.fetch( {
-      success: function( ) {
-        var ruta = routesCollection.get( 1 );
-
-        var resourceMap = resourceFormMaps[0].resourceMap;
-        var resourceMarker = resourceFormMaps[0].resourceMarker;
-
-        // If resource is not created, set centroid from route
-        if( typeof resourceViewData.id == 'undefined' ) {
-          resourceMap.setCenter(new google.maps.LatLng(ruta.get('centroid')[0],ruta.get('centroid')[1])); //(ruta.get('centroid'));
-          resourceMap.setZoom(10);
-          resourceMarker.setPosition(new google.maps.LatLng(ruta.get('centroid')[0],ruta.get('centroid')[1]));
-        }
-
-        $('input[form='+idForm+'].cgmMForm-field-locLat').val( ruta.get('centroid')[0] );
-        $('input[form='+idForm+'].cgmMForm-field-locLon').val( ruta.get('centroid')[1] );
-
-        // start and end from route
-        $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locStartLat').val(ruta.get('start')[0]);
-        $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locStartLon').val(ruta.get('start')[1]);
-
-        $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locEndLat').val(ruta.get('end')[0]);
-        $('input[form='+idForm+'].cgmMForm-field-rExtRoutes_locEndLon').val(ruta.get('end')[1]);
-
-        var route = new geozzy.rextRoutes.routeView( {
-          map: resourceMap,
-          routeModel: ruta
-        } );
-      }
-    } );
-  }
-};*/
