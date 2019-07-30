52b5217311a12ff17588f33138d2c7ade22a70c7
Carlos Zas <czas@innoto.es>
2019-07-30 13:11:18 +0200

    Corrección rextPanorama

--- distModules/rextPanorama/classes/view/templates/js/ExplorerPanoramaView.js
+++ distModules/rextPanorama/classes/view/templates/js/ExplorerPanoramaView.js
@@ -1,14 +1,20 @@
 var geozzy = geozzy || {};
-geozzy.explorerComponents = geozzy.explorerComponents || {}; //if(!geozzy.explorerComponents) geozzy.explorerComponents={};
+geozzy.explorerComponents = geozzy.explorerComponents || {};
 
-$( document ).ready( function()  {
-  var panoramaView = new geozzy.explorerComponents.panoramaView();
-  panoramaView.render();
+$(document).ready(function () {
+
+  if (geozzy.rExtPanoramaOptions.showPanorama != false ) {
+    var panoramaView = new geozzy.explorerComponents.panoramaView({
+      panoramaImage: geozzy.rExtPanoramaOptions.panoramaImg,
+      haov: geozzy.rExtPanoramaOptions.horizontalAnglePanorama,
+      vaov: geozzy.rExtPanoramaOptions.verticalAnglePanorama
+    });
+    panoramaView.render();
+  }
 } );
 
 
-geozzy.explorerComponents.panoramaView = Backbone.View.extend({
-
+geozzy.explorerComponents.panoramaView = Backbone.View.extend( {
   tpl: false,
   tplElement: false,
 
@@ -24,163 +30,34 @@ geozzy.explorerComponents.panoramaView = Backbone.View.extend({
     var that = this;
 
     var options = new Object({
-      containerDivId: 'panoramaView',
+      containerDivId: 'imgPanoramaView',
       panoramaImage: false,
       haov: 360,
       vaov: 50
     });
 
-    that.options = $.extend(true, {}, options, opts);
+    that.options = $.extend( true, {}, options, opts );
   },
-
-
-  // setParentExplorer: function( parentExplorer ) {
-  //   var  that = this;
-  //
-  //   that.parentExplorer = parentExplorer;
-  //
-  //
-  //
-  //   that.parentExplorer.bindEvent('mapResourceHover', function(ev){
-  //      that.centerInPanorama(ev.id);
-  //   });
-  //   that.parentExplorer.bindEvent('resourceMouseOut', function(ev){
-  //      //that.unShakeHotsPot(ev.id);
-  //   });
-  //
-  // },
 
   render: function() {
     var that = this;
 
-    // if( that.panorama == false ) {
+    if( that.panorama == false ) {
 
-      // var hotSpots = [];
-
-      // that.parentExplorer.resourceMinimalList.each(function(e,i){
-      //
-      //   if( e.get('panoramaPitch') &&  e.get('panoramaYaw') ) {
-      //     hotSpots.push({
-      //       "pitch": parseInt(e.get('panoramaPitch')),
-      //       "yaw": parseInt(e.get('panoramaYaw')),
-      //       "cssClass": "panorama-custom-hotspot",
-      //       "createTooltipFunc": function(a,e) {that.renderSpot(a,e);},
-      //       "createTooltipArgs": { id: e.get('id') }
-      //     });
-      //   }
-      //
-      //
-      // });
-
-      that.panorama = pannellum.viewer( that.options.containerDivId , {
+      that.panorama = pannellum.viewer( that.options.containerDivId, {
           "autoLoad": true,
-          "mouseZoom":false,
+          "mouseZoom": false,
           "showFullscreenCtrl": false,
           "type": "equirectangular",
-          "panorama": geozzy.rExtPanoramaOptions.panoramaImg,
+          "panorama": that.options.panoramaImage,
 
-          "haov": geozzy.rExtPanoramaOptions.horizontalAnglePanorama,
-          "vaov": geozzy.rExtPanoramaOptions.verticalAnglePanorama,
-          "vOffset": 10/*,
-
-          "hotSpots": hotSpots*/
+          "haov": that.options.haov,
+          "vaov": that.options.vaov,
+          "vOffset": 10
         }
       );
 
       $('#'+that.options.containerDivId).show();
-
-    // }
-
-    // that.parentExplorer.triggerEvent('onRenderListComplete',{});
+    }
   }
-
-
-
-  // renderSpot: function(hotSpotDiv, args) {
-  //   var that=this;
-  //   var e = that.parentExplorer.resourceMinimalList.get(args.id);
-  //   e.set('hotSpotDiv',hotSpotDiv);
-  //
-  //
-  //   $(hotSpotDiv).on('mouseover', function(){
-  //
-  //     that.parentExplorer.displays.map.markerBounce( args.id );
-  //     that.parentExplorer.displays.map.panTo( args.id );
-  //
-  //     that.parentExplorer.triggerEvent('resourceHover', args );
-  //     $(hotSpotDiv).addClass('panorama-custom-hotspot-selected');
-  //
-  //   });
-  //   $(hotSpotDiv).on('mouseout', function(){
-  //
-  //     that.parentExplorer.displays.map.markerOut( );
-  //     that.parentExplorer.displays.map.markerBounceEnd( args.id );
-  //
-  //     that.parentExplorer.triggerEvent('resourceMouseOut', args );
-  //     $(hotSpotDiv).removeClass('panorama-custom-hotspot-selected');
-  //   });
-  // },
-  //
-  // centerInPanorama: function(id) {
-  //   var that=this;
-  //
-  //   var e = that.parentExplorer.resourceMinimalList.get(id);
-  //   if( e.get('panoramaPitch') &&  e.get('panoramaYaw') ) {
-  //     that.panorama.lookAt(  parseInt(e.get('panoramaPitch')), parseInt(e.get('panoramaYaw'))  );
-  //     that.shakeHotspot(id);
-  //
-  //     //cogumelo.log(e.get('panoramaPitch'), e.get('panoramaYaw') );
-  //
-  //   }
-  // },
-  //
-  // shakeHotspot: function(id) {
-  //   var that = this;
-  //   var e = that.parentExplorer.resourceMinimalList.get(id);
-  //
-  //
-  //   if( e.get('panoramaPitch') &&  e.get('panoramaYaw') ) {
-  //     cogumelo.log(e.get('hotSpotDiv'));
-  //     $(e.get('hotSpotDiv')).addClass('panorama-custom-hotspot-selected');
-  //   }
-  // },
-  //
-  // unShakeHotsPot: function( id ) {
-  //   var that = this;
-  //   var e = that.parentExplorer.resourceMinimalList.get(id);
-  //
-  //
-  //   if( e.get('panoramaPitch') &&  e.get('panoramaYaw') ) {
-  //     cogumelo.log(e.get('hotSpotDiv'));
-  //     $(e.get('hotSpotDiv')).removeClass('panorama-custom-hotspot-selected');
-  //   }
-  // }
-
-
-/*
-  ,
-
-
-  resourceClick: function( element ) {
-    var that = this;
-    if(!that.parentExplorer.explorerTouchDevice){
-      that.resourceEvent( element, 'click');
-    }
-  },
-
-  resourceHover: function( element ) {
-    var that = this;
-    if(!that.parentExplorer.explorerTouchDevice){
-      that.resourceEvent( element, 'mouseenter');
-    }
-  },
-
-  resourceOut: function( element ) {
-    var that = this;
-    if(!that.parentExplorer.explorerTouchDevice){
-      that.resourceEvent( element, 'mouseleave');
-    }
-
-  }
-*/
-});
+} );
--- distModules/rextPanorama/classes/view/templates/rExtViewBlock.tpl
+++ distModules/rextPanorama/classes/view/templates/rExtViewBlock.tpl
@@ -1,50 +1,36 @@
 <!-- rExtViewBlock.tpl en rExtPanorama module -->
 <div class="rExtPanorama">
-
-  {if empty($rExt.data.panoramicImage)}
-    <script type="text/javascript">
+  <script type="text/javascript">
+    if( !cogumelo.publicConf.mod_detectMobile_isMobile ) {
       var geozzy = geozzy || {};
       geozzy.rExtPanoramaOptions = {
-        horizontalAnglePanorama: false,
-        verticalAnglePanorama: false,
-        // showPanorama: false,
-        panoramaImg: false,
-        resourceID: {$rExt.data.id}
+        showPanorama: {if !empty($rExt.data.panoramicImage)}true{else}false{/if},
+        horizontalAnglePanorama: {if !empty($rExt.data.panoramicImage)}{$rExt.data.horizontalAngleView}{else}false{/if},
+        verticalAnglePanorama: {if !empty($rExt.data.panoramicImage)}{$rExt.data.verticalAngleView}{else}false{/if},
+        panoramaImg: {if !empty($rExt.data.panoramicImage)}cogumelo.publicConf.site_host+"/cgmlImg/{$rExt.data.panoramicImage.id}-a{$rExt.data.panoramicImage.aKey}/panoramica/panorama.jpg"{else}false{/if}
       };
-    </script>
-  {else}
-    <script type="text/javascript">
-      var geozzy = geozzy || {};
-      if( !cogumelo.publicConf.mod_detectMobile_isMobile ) {
-        geozzy.rExtPanoramaOptions = {
-          horizontalAnglePanorama: {$rExt.data.horizontalAngleView},
-          verticalAnglePanorama: {$rExt.data.verticalAngleView},
-          // showPanorama: true,
-          panoramaImg: cogumelo.publicConf.site_host+"/cgmlImg/{$rExt.data.panoramicImage.id}-a{$rExt.data.panoramicImage.aKey}/panorama/panorama.jpg",
-          resourceID: {$rExt.data.id}
-        };
+    }
+  </script>
+
+  <style>
+    {literal}
+      .panorama-custom-hotspot{
+        height: 15px;
+        width: 15px;
+        border-radius: 50%;
+        background: #D35E5E;
+        border: 2px solid white;
       }
-    </script>
-    <style>
-      {literal}
-        .panorama-custom-hotspot{
-          height: 15px;
-          width: 15px;
-          border-radius: 50%;
-          background: #D35E5E;
-          border: 2px solid white;
-        }
 
-        .panorama-custom-hotspot-selected{
-          border-width: 3px;
-          margin-left: -1px;
-          margin-top: -1px;
-        }
-      {/literal}
-    </style>
-  {/if}
+      .panorama-custom-hotspot-selected{
+        border-width: 3px;
+        margin-left: -1px;
+        margin-top: -1px;
+      }
+    {/literal}
+  </style>
 
-  <div id="panoramaView" style="display:none;height:350px;background-color:grey;"></div>
+  <div id="imgPanoramaView" style="display:none;height:350px;background-color:grey;"></div>
 
 </div>
 <!-- /rExtViewBlock.tpl en rExtPanorama module -->
